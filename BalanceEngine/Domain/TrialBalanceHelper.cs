75ffdfb225580e626fba181c6b31e5c37503f85d
José Manuel Cota <jmcota@ontica.org>
2021-06-07 11:41:27 -0500

    Rfx Implement a class with helper methods to build trial balances

---
+++ BalanceEngine/Domain/TrialBalanceHelper.cs
@@ -0,0 +1,185 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Helper methods                          *
+*  Type     : TrialBalanceHelper                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Helper methods to build trial balances and related accounting information.                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Empiria.Collections;
+
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+using Empiria.FinancialAccounting.BalanceEngine.Data;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Helper methods to build trial balances and related accounting information.</summary>
+  internal class TrialBalanceHelper {
+
+    private readonly TrialBalanceCommand _command;
+
+    internal TrialBalanceHelper(TrialBalanceCommand command) {
+      _command = command;
+    }
+
+
+    internal FixedList<TrialBalanceEntry> CombineSummaryAndPostingEntries(List<TrialBalanceEntry> summaryEntries,
+                                                                          FixedList<TrialBalanceEntry> postingEntries) {
+      var returnedEntries = new List<TrialBalanceEntry>(postingEntries);
+
+      returnedEntries.AddRange(summaryEntries);
+
+      returnedEntries = returnedEntries.OrderBy(a => a.Ledger.Number)
+                                       .ThenBy(a => a.Currency.Code)
+                                       .ThenByDescending(a => a.Account.DebtorCreditor)
+                                       .ThenBy(a => a.Account.Number)
+                                       .ThenBy(a => a.Sector.Code)
+                                       .ThenBy(a => a.SubledgerAccountId)
+                                       .ToList();
+
+      return returnedEntries.ToFixedList();
+    }
+
+
+    internal FixedList<TrialBalanceEntry> ConsolidateToTargetCurrency(FixedList<TrialBalanceEntry> trialBalance) {
+      var targetCurrency = Currency.Parse(_command.ValuateToCurrrencyUID);
+
+      var summaryEntries = new EmpiriaHashTable<TrialBalanceEntry>();
+
+      foreach (var entry in trialBalance) {
+        string hash = $"{entry.Account.Number}||{entry.Sector.Code}||{targetCurrency.Id}||{entry.Ledger.Id}";
+
+        if (entry.Currency.Equals(targetCurrency)) {
+          summaryEntries.Insert(hash, entry);
+        } else if (summaryEntries.ContainsKey(hash)) {
+          summaryEntries[hash].Sum(entry);
+        } else {
+          entry.Currency = targetCurrency;
+          summaryEntries.Insert(hash, entry);
+        }
+      }
+
+      return summaryEntries.Values.ToList()
+                                  .ToFixedList();
+    }
+
+
+    internal List<TrialBalanceEntry> GenerateSummaryEntries(FixedList<TrialBalanceEntry> entries) {
+      var summaryEntries = new EmpiriaHashTable<TrialBalanceEntry>(entries.Count);
+
+      foreach (var entry in entries) {
+        Account currentParent;
+
+        if (_command.ReturnSubledgerAccounts) {
+          currentParent = entry.Account;
+
+        } else if (_command.DoNotReturnSubledgerAccounts && entry.Account.HasParent) {
+          currentParent = entry.Account.GetParent();
+
+        } else if (_command.DoNotReturnSubledgerAccounts && entry.Account.NotHasParent) {
+          continue;
+        } else {
+          throw Assertion.AssertNoReachThisCode();
+        }
+
+        while (true) {
+
+          GenerateOrIncreaseSummaryEntry(summaryEntries, entry, currentParent, entry.Sector);
+
+          if (!currentParent.HasParent && entry.Sector.Code != "00") {
+            GenerateOrIncreaseSummaryEntry(summaryEntries, entry, currentParent, Sector.Empty);
+            break;
+
+          } else if (!currentParent.HasParent) {
+            break;
+
+          } else {
+            currentParent = currentParent.GetParent();
+          }
+
+        } // while
+
+      } // foreach
+
+      return summaryEntries.Values.ToList();
+    }
+
+
+    internal FixedList<TrialBalanceEntry> GetTrialBalanceEntries() {
+      TrialBalanceCommandData commandData = _command.MapToTrialBalanceCommandData();
+
+      return TrialBalanceDataService.GetTrialBalanceEntries(commandData);
+    }
+
+
+    internal FixedList<TrialBalanceEntry> RestrictLevels(FixedList<TrialBalanceEntry> entries) {
+      if (_command.Level == 0) {
+        return entries;
+      }
+
+      if (_command.DoNotReturnSubledgerAccounts) {
+        return entries.FindAll(x => x.Level <= _command.Level);
+      } else if (_command.ReturnSubledgerAccounts) {
+        return entries.FindAll(x => x.Level <= _command.Level || x.LedgerAccountId != 0);
+      } else {
+        throw Assertion.AssertNoReachThisCode();
+      }
+    }
+
+
+    internal FixedList<TrialBalanceEntry> ValuateToExchangeRate(FixedList<TrialBalanceEntry> entries) {
+      var exchangeRateType = ExchangeRateType.Parse(_command.ExchangeRateTypeUID);
+
+      FixedList<ExchangeRate> exchageRates = ExchangeRate.GetList(exchangeRateType, _command.ExchangeRateDate);
+
+      foreach (var entry in entries) {
+        var exchangeRate = exchageRates.FirstOrDefault(a => a.FromCurrency.Code == _command.ValuateToCurrrencyUID &&
+                                                            a.ToCurrency.Code == entry.Currency.Code);
+
+        Assertion.AssertObject(exchangeRate, $"No hay tipo de cambio para la moneda {entry.Currency.FullName}.");
+
+        entry.MultiplyBy(exchangeRate.Value);
+      }
+      return entries;
+    }
+
+    #region Private methods
+
+    private void GenerateOrIncreaseSummaryEntry(EmpiriaHashTable<TrialBalanceEntry> summaryEntries,
+                                            TrialBalanceEntry entry,
+                                            Account targetAccount, Sector targetSector) {
+
+      string hash = $"{targetAccount.Number}||{targetSector.Code}||{entry.Currency.Id}||{entry.Ledger.Id}";
+
+      TrialBalanceEntry summaryEntry;
+
+      summaryEntries.TryGetValue(hash, out summaryEntry);
+
+      if (summaryEntry == null) {
+
+        summaryEntry = new TrialBalanceEntry() {
+          Ledger = entry.Ledger,
+          Currency = entry.Currency,
+          Sector = targetSector,
+          Account = targetAccount,
+          ItemType = "BalanceSummary"
+        };
+        summaryEntry.Sum(entry);
+
+        summaryEntries.Insert(hash, summaryEntry);
+
+      } else {
+        summaryEntry.Sum(entry);
+      }
+    }
+
+    #endregion Private methods
+
+  }  // class TrialBalanceHelper
+
+}  // namespace Empiria.FinancialAccounting.BalanceEngine
