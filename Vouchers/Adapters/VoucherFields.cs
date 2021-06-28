a1cabea3825fdbd474b4e98ba833c65e1d85aee5
José Manuel Cota <jmcota@ontica.org>
2021-06-28 16:33:48 -0400

    VoucherFields adapter added

---
+++ Vouchers/Adapters/VoucherFields.cs
@@ -0,0 +1,17 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Vouchers Management                        Component : Interface adapters                      *
+*  Assembly : FinancialAccounting.Vouchers.dll           Pattern   : Input Data Holder                       *
+*  Type     : VoucherFields                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Data structure that serves as an adapter to update vouchers data.                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.FinancialAccounting.Vouchers.Adapters {
+
+  public class VoucherFields {
+
+  }  // class VoucherFields
+
+}  // namespace Empiria.FinancialAccounting.Vouchers.Adapters
