688f92a90605710dde56fd9f934bf45c2c366675
José Manuel Cota <jmcota@ontica.org>
2021-09-09 21:24:58 -0400

    Financial Reports module added

---
+++ FinancialReports/Empiria.FinancialAccounting.FinancialReports.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                 Module  : Financial Reports                       *
+*  Assembly : FinancialAccounting.FinancialReports.dll     Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : This module provides services for financial reports generation.                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ FinancialReports/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Financial Accounting Financial Reports Module
+
+This module provides services for financial reports generation.
+
+## License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+## Copyright
+
+Copyright © 2000-2021. La Vía Óntica SC, Ontica LLC and colaborators.
