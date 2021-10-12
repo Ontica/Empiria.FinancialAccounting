18f6fb1e24d55efbf307343320906524efcae9b7
José Manuel Cota <jmcota@ontica.org>
2021-10-11 21:39:24 -0400

    Get ImportVouchersTotals from a database read

--- BanobrasIntegration/VouchersImporter/DbTablesImporter/DbVouchersImporterDataService.cs
+++ BanobrasIntegration/VouchersImporter/DbTablesImporter/DbVouchersImporterDataService.cs
@@ -8,8 +8,11 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+using System.Collections.Generic;
 
 using Empiria.Data;
+using Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter.Adapters;
+using Empiria.FinancialAccounting.Vouchers;
 
 namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
 
@@ -18,22 +21,56 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
 
     static readonly DateTime DB_START_IMPORTATION_DATE = new DateTime(2021, 8, 1);
 
-    static internal FixedList<Encabezado> GetEncabezados() {
+    static internal List<Encabezado> GetEncabezados() {
       var sql = "SELECT * FROM VW_MC_ENCABEZADOS " +
                $"WHERE ENC_FECHA_VOL >= '{CommonMethods.FormatSqlDate(DB_START_IMPORTATION_DATE)}'";
 
       var op = DataOperation.Parse(sql);
 
-      return DataReader.GetPlainObjectFixedList<Encabezado>(op);
+      return DataReader.GetPlainObjectList<Encabezado>(op);
     }
 
-    static internal FixedList<Movimiento> GetMovimientos() {
+
+    static internal List<Movimiento> GetMovimientos() {
       var sql = "SELECT * FROM VW_MC_MOVIMIENTOS " +
-         $"WHERE MCO_FECHA_VOL >= '{CommonMethods.FormatSqlDate(DB_START_IMPORTATION_DATE)}'";
+               $"WHERE MCO_FECHA_VOL >= '{CommonMethods.FormatSqlDate(DB_START_IMPORTATION_DATE)}'";
 
       var op = DataOperation.Parse(sql);
 
-      return DataReader.GetPlainObjectFixedList<Movimiento>(op);
+      return DataReader.GetPlainObjectList<Movimiento>(op);
+    }
+
+    static internal ImportVouchersResult GetEncabezadosTotals() {
+      var sql = "SELECT ENC_SISTEMA, COUNT(*) AS TOTAL " +
+                "FROM VW_MC_ENCABEZADOS " +
+               $"WHERE ENC_FECHA_VOL >= '{CommonMethods.FormatSqlDate(DB_START_IMPORTATION_DATE)}' " +
+                "GROUP BY ENC_SISTEMA ";
+
+
+      var op = DataOperation.Parse(sql);
+
+      var view = DataReader.GetDataView(op);
+
+      var list = new List<ImportVouchersTotals>();
+
+      for (int i = 0; i < view.Count; i++) {
+        var system = TransactionalSystem.Get(x => x.SourceSystemId == (int) (view[i]["ENC_SISTEMA"]));
+
+        var totals = new ImportVouchersTotals {
+          Description = system.Name,
+          UID = system.UID,
+          VouchersCount = (int) (decimal) view[i]["TOTAL"]
+        };
+        list.Add(totals);
+      }
+
+      var result = new ImportVouchersResult();
+
+      list.Sort((x, y) => x.Description.CompareTo(y.Description));
+
+      result.VoucherTotals = list.ToFixedList();
+
+      return result;
     }
 
   }  // class DbVouchersImporterDataService
