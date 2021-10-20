1030a84d4066f2b43b460b0669f496b0fcc24bb2
José Manuel Cota <jmcota@ontica.org>
2021-10-19 22:54:18 -0400

    TextFileStructurer was completed

--- BanobrasIntegration/VouchersImporter/TextFileImporter/TextFileStructurer.cs
+++ BanobrasIntegration/VouchersImporter/TextFileImporter/TextFileStructurer.cs
@@ -8,8 +8,10 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
+using Empiria.FinancialAccounting.Vouchers;
 using Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter.Adapters;
 
 namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
@@ -29,9 +31,7 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
       _command = command;
       _textFileLines = textFileLines;
 
-      EnsureFileLinesAreValid();
-
-      _entries = GetEntries();
+      _entries = ParseTextLinesToTextFileVoucherEntries();
     }
 
 
@@ -41,26 +41,108 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
       }
     }
 
+    #region Methods
 
-    public int LinesCount {
-      get {
-        return _textFileLines.Length;
+    internal FixedList<ToImportVoucher> GetToImportVouchersList() {
+      string[] voucherUniqueIDsArray = GetVoucherUniqueIds();
+
+      var vouchersListToImport = new List<ToImportVoucher>(voucherUniqueIDsArray.Length);
+
+      foreach (string voucherUniqueID in voucherUniqueIDsArray) {
+        ToImportVoucher voucherToImport = BuildVoucherToImport(voucherUniqueID);
+
+        vouchersListToImport.Add(voucherToImport);
       }
+
+      return vouchersListToImport.ToFixedList();
     }
 
-    #region Private methods
 
-    private void EnsureFileLinesAreValid() {
-      Assertion.Assert(this.LinesCount >= 2, "El archivo de texto contiene menos de dos registros.");
+    private ToImportVoucher BuildVoucherToImport(string voucherUniqueID) {
+      ToImportVoucherHeader header = MapToImportVoucherHeader(voucherUniqueID);
+      FixedList<ToImportVoucherEntry> entries = MapToImportVoucherEntries(header);
+
+      return new ToImportVoucher(header, entries);
     }
 
-    private FixedList<TextFileVoucherEntry> GetEntries() {
-      var accountsChart = AccountsChart.Parse(_command.AccountsChartUID);
 
-      return new FixedList<TextFileVoucherEntry>(_textFileLines.Select(x => new TextFileVoucherEntry(accountsChart.MasterData, x)));
+    private ToImportVoucherHeader MapToImportVoucherHeader(string voucherUniqueID) {
+      var sourceHeader = _entries.Find(x => x.VoucherUniqueID == voucherUniqueID);
+
+      var header = new ToImportVoucherHeader();
+
+      header.ImportationSet = sourceHeader.GetImportationSet();
+      header.UniqueID = sourceHeader.VoucherUniqueID;
+      header.Ledger = sourceHeader.GetLedger();
+      header.Concept = sourceHeader.GetConcept();
+      header.AccountingDate = sourceHeader.GetAccountingDate();
+      header.VoucherType = VoucherType.Parse(_command.VoucherTypeUID);
+      header.TransactionType = TransactionType.Parse(_command.TransactionTypeUID);
+      header.FunctionalArea = sourceHeader.GetFunctionalArea();
+      header.RecordingDate = DateTime.Today;
+      header.ElaboratedBy = Participant.Current;
+
+      header.Issues = sourceHeader.GetHeaderIssues();
+
+      return header;
     }
 
-    #endregion Private methods
+
+    private FixedList<ToImportVoucherEntry> MapToImportVoucherEntries(ToImportVoucherHeader header) {
+      var entries = _entries.FindAll(x => x.VoucherUniqueID == header.UniqueID);
+
+      var mapped = entries.Select(entry => MapToImportVoucherEntry(header, entry));
+
+      return new List<ToImportVoucherEntry>(mapped).ToFixedList();
+    }
+
+
+    private ToImportVoucherEntry MapToImportVoucherEntry(ToImportVoucherHeader header,
+                                                         TextFileVoucherEntry sourceEntry) {
+      var entry = new ToImportVoucherEntry(header);
+
+      entry.StandardAccount = sourceEntry.GetStandardAccount();
+      entry.Sector = sourceEntry.GetSector();
+      entry.SubledgerAccount = sourceEntry.GetSubledgerAccount();
+      entry.SubledgerAccountNo = sourceEntry.GetSubledgerAccountNo();
+      entry.ResponsibilityArea = sourceEntry.GetResponsibilityArea();
+      entry.BudgetConcept = string.Empty;
+      entry.EventType = sourceEntry.GetEventType();
+      entry.VerificationNumber = string.Empty;
+      entry.VoucherEntryType = sourceEntry.GetVoucherEntryType();
+      entry.Date = header.AccountingDate;
+      entry.Concept = sourceEntry.GetConcept();
+      entry.Currency = sourceEntry.GetCurrency();
+      entry.Amount = sourceEntry.GetAmount();
+      entry.ExchangeRate = sourceEntry.GetExchangeRate();
+      entry.BaseCurrencyAmount = sourceEntry.GetBaseCurrencyAmount();
+      entry.Protected = false;
+
+      entry.Issues = sourceEntry.GetEntryIssues();
+
+      return entry;
+    }
+
+
+    private FixedList<TextFileVoucherEntry> ParseTextLinesToTextFileVoucherEntries() {
+      List<TextFileVoucherEntry> entries = new List<TextFileVoucherEntry>(_textFileLines.Length);
+
+      for (int lineIndex = 0; lineIndex < _textFileLines.Length; lineIndex++) {
+        entries.Add(new TextFileVoucherEntry(_textFileLines[lineIndex], lineIndex + 1));
+      }
+
+      return entries.ToFixedList();
+    }
+
+
+    private string[] GetVoucherUniqueIds() {
+      return this.Entries.Select(x => x.VoucherUniqueID)
+                         .Distinct()
+                         .ToArray();
+    }
+
+
+    #endregion Methods
 
   }  // class TextFileStructurer
 
