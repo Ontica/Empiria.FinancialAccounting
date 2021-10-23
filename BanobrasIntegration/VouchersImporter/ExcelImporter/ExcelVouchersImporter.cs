db4e300331780dcbadce169459ae25f81e4ba58c
José Manuel Cota <jmcota@ontica.org>
2021-10-23 19:10:39 -0400

    Excel voucher importer completed

--- BanobrasIntegration/VouchersImporter/ExcelImporter/ExcelVouchersImporter.cs
+++ BanobrasIntegration/VouchersImporter/ExcelImporter/ExcelVouchersImporter.cs
@@ -8,11 +8,10 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 using Empiria.FinancialAccounting.Vouchers;
-using Empiria.FinancialAccounting.Vouchers.Adapters;
-using Empiria.FinancialAccounting.Vouchers.UseCases;
 
 using Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter.Adapters;
 
@@ -22,48 +21,81 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.VouchersImporter {
   internal class ExcelVouchersImporter {
 
     private readonly ImportVouchersCommand _command;
-    private readonly FileInfo _excelFile;
+    private readonly FixedList<ExcelVoucherEntry> _excelFileEntries;
 
     internal ExcelVouchersImporter(ImportVouchersCommand command, FileInfo excelFile) {
       Assertion.AssertObject(command, "command");
       Assertion.AssertObject(excelFile, "excelFile");
 
       _command = command;
-      _excelFile = excelFile;
+
+      _excelFileEntries = ReadExcelContent(excelFile);
     }
 
 
     internal ImportVouchersResult DryRunImport() {
-      return new ImportVouchersResult();
+      VoucherImporter voucherImporter = GetVoucherImporter();
+
+      return voucherImporter.DryRunImport();
     }
 
 
     internal ImportVouchersResult DryRunImport(Voucher voucher) {
-      return new ImportVouchersResult();
+      VoucherImporter voucherImporter = GetVoucherImporter(voucher);
+
+      return voucherImporter.DryRunImport();
     }
 
 
     internal ImportVouchersResult Import() {
-      return new ImportVouchersResult();
+      VoucherImporter voucherImporter = GetVoucherImporter();
+
+      return voucherImporter.Import();
     }
 
 
     internal ImportVouchersResult Import(Voucher voucher) {
-      try {
-        var importer = new ExcelFileStructurer(voucher, _excelFile);
+      VoucherImporter voucherImporter = GetVoucherImporter(voucher);
 
-        FixedList<VoucherEntryFields> entries = importer.GetVoucherEntries();
+      return voucherImporter.Import();
+    }
 
-        using (var usecases = VoucherEditionUseCases.UseCaseInteractor()) {
-          usecases.AppendEntries(voucher.Id, entries);
-        }
 
-      } catch (Exception e) {
-        EmpiriaLog.Error(e);
-        throw;
+    private VoucherImporter GetVoucherImporter() {
+      var structurer = new ExcelFileStructurer(_command, _excelFileEntries);
+
+      FixedList<ToImportVoucher> toImport = structurer.GetToImportVouchersList();
+
+      return new VoucherImporter(_command, toImport);
+    }
+
+
+    private VoucherImporter GetVoucherImporter(Voucher voucher) {
+      var structurer = new ExcelFileStructurer(_command, _excelFileEntries);
+
+      FixedList<ToImportVoucher> toImport = structurer.GetToImportVouchersList();
+
+      return new VoucherImporter(_command, toImport);
+    }
+
+
+    private FixedList<ExcelVoucherEntry> ReadExcelContent(FileInfo excelFileInfo) {
+      var excelFile = new ExcelFileHandler(excelFileInfo);
+
+      var allEntries = new List<ExcelVoucherEntry>();
+
+      string[] worksheets = excelFile.SelectCandidateWorksheets();
+
+      Assertion.Assert(worksheets.Length > 0,
+                      "El archivo Excel NO tiene información de pólizas para importar en ninguna de sus hojas");
+
+      foreach (var worksheet in worksheets) {
+        FixedList<ExcelVoucherEntry> worksheetEntries = excelFile.GetWorksheetEntries(worksheet);
+
+        allEntries.AddRange(worksheetEntries);
       }
 
-      return new ImportVouchersResult();
+      return allEntries.ToFixedList();
     }
 
   }  // class ExcelVouchersImporter
