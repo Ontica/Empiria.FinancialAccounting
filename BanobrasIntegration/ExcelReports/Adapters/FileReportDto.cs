e85ac39c8caff4aec9ed3584d43a63127ebff18d
efrain-jerv <efrain.rodriguez@ontica.org>
2021-10-25 23:37:56 -0700

    ExcelFileDto class renaming by FileReportDto

---
+++ BanobrasIntegration/ExcelReports/Adapters/FileReportDto.cs
@@ -0,0 +1,29 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Banobras Integration Services                Component : File Reports                          *
+*  Assembly : FinancialAccounting.BanobrasIntegration.dll  Pattern   : Data Transfer Object                  *
+*  Type     : FileReportDto                                 License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : DTO that returns an Excel file data.                                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.FinancialAccounting.BanobrasIntegration.ExcelReports.Adapters {
+
+  public class FileReportDto {
+
+    internal FileReportDto() {
+      // no-op
+    }
+
+    public string Url {
+      get; internal set;
+    }
+
+    public string Type {
+      get; internal set;
+    }
+
+  }  // class FileReportDto
+
+}  // namespace Empiria.FinancialAccounting.BanobrasIntegration.ExcelReports.Adapters
