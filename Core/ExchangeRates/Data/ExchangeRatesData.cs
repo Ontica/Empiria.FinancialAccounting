f82a9209b22051979e27dd7b1dfb1a6e2ad7278d
José Manuel Cota <jmcota@ontica.org>
2021-12-06 19:22:02 -0500

    Add an in-memory cache for exchange rates lists

--- Core/ExchangeRates/Data/ExchangeRatesData.cs
+++ Core/ExchangeRates/Data/ExchangeRatesData.cs
@@ -1,7 +1,7 @@
 ﻿/* Empiria Financial *****************************************************************************************
 *                                                                                                            *
 *  Module   : Exchange Rates                             Component : Data Access Layer                       *
-*  Assembly : FinancialAccounting.Core.dll               Pattern   : Data Service                            *
+*  Assembly : FinancialAccounting.Core.dll               Pattern   : Data Service with Cache                 *
 *  Type     : ExchangeRatesData                          License   : Please read LICENSE.txt file            *
 *                                                                                                            *
 *  Summary  : Data access layer for exchange rates.                                                          *
@@ -9,6 +9,7 @@
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 
+using Empiria.Collections;
 using Empiria.Data;
 
 namespace Empiria.FinancialAccounting.Data {
@@ -16,8 +17,18 @@ namespace Empiria.FinancialAccounting.Data {
   /// <summary>Data access layer for exchange rates.</summary>
   static internal class ExchangeRatesData {
 
+    static readonly EmpiriaHashTable<FixedList<ExchangeRate>> _cache =
+                                                  new EmpiriaHashTable<FixedList<ExchangeRate>>();
+
+
     static internal FixedList<ExchangeRate> GetExchangeRates(ExchangeRateType exchangeRateType,
                                                              DateTime date) {
+      string hashKey = GetHashKey(exchangeRateType, date);
+
+      if (_cache.ContainsKey(hashKey)) {
+        return _cache[hashKey];
+      }
+
       var sql = "SELECT * FROM AO_EXCHANGE_RATES " +
                 $"WHERE EXCHANGE_RATE_TYPE_ID = {exchangeRateType.Id} " +
                 $"AND FROM_DATE = '{CommonMethods.FormatSqlDate(date)}' " +
@@ -25,9 +36,12 @@ namespace Empiria.FinancialAccounting.Data {
 
       var dataOperation = DataOperation.Parse(sql);
 
-      return DataReader.GetFixedList<ExchangeRate>(dataOperation);
-    }
+      var list = DataReader.GetFixedList<ExchangeRate>(dataOperation);
 
+      _cache.Insert(hashKey, list);
+
+      return _cache[hashKey];
+    }
 
     static internal FixedList<ExchangeRate> GetExchangeRates(DateTime date) {
       var sql = "SELECT * FROM AO_EXCHANGE_RATES " +
@@ -39,6 +53,10 @@ namespace Empiria.FinancialAccounting.Data {
       return DataReader.GetFixedList<ExchangeRate>(dataOperation);
     }
 
+
+    static private string GetHashKey(ExchangeRateType exchangeRateType, DateTime date) {
+      return $"{exchangeRateType.UID}||{date.ToString("dd/MM/yyyy")}";
+    }
 
   }  // class ExchangeRatesData
 
