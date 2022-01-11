09ef1d008f7d9767e91c52a65c43106b47fbcc55
efrain-jerv <efrain.rodriguez@ontica.org>
2022-01-10 19:09:55 -0700

    Equivalencia M.N. column renamed to Equivalencia en dolares

--- BalanceEngine/Domain/TrialBalance.cs
+++ BalanceEngine/Domain/TrialBalance.cs
@@ -184,7 +184,7 @@ namespace Empiria.FinancialAccounting.BalanceEngine {
       columns.Add(new DataTableColumn("totalBalance", "Importe Mon. Ext.", "decimal"));
       //columns.Add(new DataTableColumn("exchangeRate", "Valor M.N.", "decimal", 6));
       columns.Add(new DataTableColumn("valuedExchangeRate", "Tipo cambio", "decimal", 6));
-      columns.Add(new DataTableColumn("totalEquivalence", "Equivalencia M.N.", "decimal"));
+      columns.Add(new DataTableColumn("totalEquivalence", "Equivalencia en dolares", "decimal"));
 
 
       return columns.ToFixedList();
