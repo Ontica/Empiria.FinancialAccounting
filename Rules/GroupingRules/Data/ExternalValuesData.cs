ac4aacf7edcaa059bb6463a8ce5f2c63d33c53e2
José Manuel Cota <jmcota@ontica.org>
2022-01-29 21:03:26 -0500

    External values data moved to COF_CONCEPTOS_VARIABLES

--- Rules/GroupingRules/Data/ExternalValuesData.cs
+++ Rules/GroupingRules/Data/ExternalValuesData.cs
@@ -38,12 +38,11 @@ namespace Empiria.FinancialAccounting.Rules.Data {
     }
 
     static internal ExternalValue GetValue(string externalVariableCode, DateTime date) {
-      var sql = "SELECT * FROM COF_CONCEPTOS_VARIABLES " +
+      var sql = "SELECT * FROM COF_CONCEPTOS_VALORES " +
                $"WHERE CLAVE_VARIABLE = '{externalVariableCode}' " +
                $"AND FECHA = '{CommonMethods.FormatSqlDate(date)}'";
 
       var dataOperation = DataOperation.Parse(sql);
-
 
       return DataReader.GetObject<ExternalValue>(dataOperation, ExternalValue.Empty);
     }
