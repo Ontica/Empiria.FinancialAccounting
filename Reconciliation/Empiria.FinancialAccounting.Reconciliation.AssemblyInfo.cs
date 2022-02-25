8b094e6f0220e2857d11abb3b50040e91a70e592
José Manuel Cota <jmcota@ontica.org>
2022-02-24 23:21:09 -0500

    Balances Reconciliation module added

---
+++ Reconciliation/Empiria.FinancialAccounting.Reconciliation.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                 Module  : Reconciliation Services                 *
+*  Assembly : FinancialAccounting.Reconciliation.dll       Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : This module provides financial account's balance reconciliation services.                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Reconciliation/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Financial Accounting Reconciliation Module
+
+This module provides account's balance reconciliation services.
+
+## License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+## Copyright
+
+Copyright © 2000-2022. La Vía Óntica SC, Ontica LLC and colaborators.
