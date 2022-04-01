e6ba16a4b00e4b5e0229699f5f883c850079483b
José Manuel Cota <jmcota@ontica.org>
2022-04-01 15:31:22 -0600

    Voucher builder for 'Nivelación de cuentas de compraventa' added

--- Vouchers/Domain/SpecialCases/NivelacionCuentasCompraventaVoucherBuilder.cs
+++ Vouchers/Domain/SpecialCases/NivelacionCuentasCompraventaVoucherBuilder.cs
@@ -10,6 +10,9 @@
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 using System.Collections.Generic;
+
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+using Empiria.FinancialAccounting.BalanceEngine.UseCases;
 using Empiria.FinancialAccounting.Vouchers.Adapters;
 
 namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
@@ -49,35 +52,16 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
 
 
     private void CreateVoucherEntries(Voucher voucher, FixedList<VoucherEntryFields> entries) {
-      foreach (var entry in entries) {
+      foreach (var entryFields in entries) {
 
-        entry.VoucherId = voucher.Id;
+        entryFields.VoucherId = voucher.Id;
 
-        VoucherEntry cancelationEntry = voucher.AppendEntry(entry);
+        VoucherEntry voucherEntry = voucher.AppendEntry(entryFields);
 
-        cancelationEntry.Save();
+        voucherEntry.Save();
       }
     }
 
-
-    private AccountsList GetNivelacionAccountsList() {
-      return base.SpecialCaseType.AccountsList;
-    }
-
-
-    private FixedList<VoucherEntryFields> BuildVoucherEntries() {
-      var accountsListItems = base.SpecialCaseType.AccountsList.GetItems();
-
-      var entries = new List<VoucherEntryFields>();
-
-      foreach (var item in accountsListItems) {
-        var entry = new VoucherEntryFields();
-
-        entries.Add(entry);
-      }
-
-      return entries.ToFixedList();
-    }
 
     private FixedList<string> ImplementsDryRun(FixedList<VoucherEntryFields> entries) {
       var validator = new VoucherValidator(Ledger.Parse(base.Fields.LedgerUID),
@@ -85,6 +69,132 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
 
       return validator.Validate(entries);
     }
+
+
+    private TrialBalanceCommand BuildBalancesCommand() {
+      return new TrialBalanceCommand {
+        TrialBalanceType = BalanceEngine.TrialBalanceType.Balanza,
+        AccountsChartUID = base.Fields.AccountsChartUID,
+        BalancesType = BalanceEngine.BalancesType.WithCurrentBalanceOrMovements,
+        ShowCascadeBalances = true,
+        InitialPeriod = new TrialBalanceCommandPeriod {
+          FromDate = base.Fields.CalculationDate,
+          ToDate = base.Fields.CalculationDate
+        }
+      };
+    }
+
+
+    private FixedList<TrialBalanceEntryDto> BuildTrialBalanceEntries() {
+      using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
+        TrialBalanceCommand command = BuildBalancesCommand();
+
+        var entries = usecases.BuildTrialBalance(command).Entries;
+
+        return new FixedList<TrialBalanceEntryDto>(entries.Select(x => (TrialBalanceEntryDto) x));
+      }
+    }
+
+
+    private FixedList<VoucherEntryFields> BuildVoucherEntries() {
+      FixedList<AccountsListItem> accountsListItems = GetNivelacionAccountsList();
+
+      FixedList<TrialBalanceEntryDto> balances = BuildTrialBalanceEntries();
+
+      FixedList<ExchangeRate> exchangeRates = GetValuationExchangeRates();
+
+      var entries = new List<VoucherEntryFields>();
+
+      foreach (var item in accountsListItems) {
+        decimal registeredBalance = GetBalance(item.AccountNumber, balances);
+
+        decimal revaluatedBalance = RevaluateForeignCurrencyBalances(item.TargetAccountNumber, balances, exchangeRates);
+
+        if (registeredBalance != 0 && registeredBalance != revaluatedBalance) {
+          entries.AddRange(BuildVoucherEntryFieldsList(item, registeredBalance, revaluatedBalance));
+        }
+      }
+
+      Assertion.Assert(entries.Count >= 2, "Las cuentas de compraventa ya están niveladas en la fecha proporcionada.");
+
+      return entries.ToFixedList();
+    }
+
+
+    private VoucherEntryFields BuildVoucherEntryFields(string accountNumber, decimal amount, bool isDebit) {
+      StandardAccount stdAccount = AccountsChart.Parse(base.Fields.AccountsChartUID).GetStandardAccount(accountNumber);
+      LedgerAccount ledgerAccount = Ledger.Parse(base.Fields.LedgerUID).AssignAccount(stdAccount);
+
+      return new VoucherEntryFields {
+        Amount = amount,
+        BaseCurrencyAmount = amount,
+        CurrencyUID = "01",
+        SectorId = Sector.Empty.Id,
+        SubledgerAccountNumber = String.Empty,
+        StandardAccountId = stdAccount.Id,
+        LedgerAccountId = ledgerAccount.Id,
+        VoucherEntryType = isDebit ? VoucherEntryType.Debit : VoucherEntryType.Credit
+      };
+    }
+
+
+    private List<VoucherEntryFields> BuildVoucherEntryFieldsList(AccountsListItem item,
+                                                                 decimal registeredBalance,
+                                                                 decimal revaluatedBalance) {
+      var entries = new List<VoucherEntryFields>();
+
+      if (registeredBalance > revaluatedBalance) {
+        entries.Add(BuildVoucherEntryFields(item.AccountNumber, registeredBalance - revaluatedBalance, false));
+        entries.Add(BuildVoucherEntryFields(item.LossAccount, registeredBalance - revaluatedBalance, true));
+      } else {
+        entries.Add(BuildVoucherEntryFields(item.AccountNumber, revaluatedBalance - registeredBalance, true));
+        entries.Add(BuildVoucherEntryFields(item.ProfitAccount, revaluatedBalance - registeredBalance, false));
+      }
+
+      return entries;
+    }
+
+
+    private decimal GetBalance(string accountNumber, FixedList<TrialBalanceEntryDto> balances) {
+      var balance = balances.Find(x => x.AccountNumber == accountNumber && x.LedgerUID == base.Fields.LedgerUID);
+
+      if (balance == null) {
+        return 0;
+      }
+      return balance.CurrentBalance.Value;
+    }
+
+
+    private FixedList<AccountsListItem> GetNivelacionAccountsList() {
+      return base.SpecialCaseType.AccountsList.GetItems();
+    }
+
+
+    private FixedList<ExchangeRate> GetValuationExchangeRates() {
+      var exchangeRateType = ExchangeRateType.Parse("96c617f6-8ed9-47f3-8d2d-f1240e446e1d");
+
+      return ExchangeRate.GetList(exchangeRateType, base.Fields.CalculationDate);
+    }
+
+
+    private decimal RevaluateForeignCurrencyBalances(string foreignCurrencyAccountNumber,
+                                                     FixedList<TrialBalanceEntryDto> balances,
+                                                     FixedList<ExchangeRate> exchangeRates) {
+      FixedList<TrialBalanceEntryDto> foreignBalances = balances.FindAll(x => x.AccountNumber == foreignCurrencyAccountNumber &&
+                                                                              x.LedgerUID == base.Fields.LedgerUID);
+
+      decimal revaluatedForeignBalance = 0;
+
+      foreach (var foreignBalance in foreignBalances) {
+        var currency = Currency.Parse(foreignBalance.CurrencyCode);
+        var exchangeRate = exchangeRates.Find(x => x.ToCurrency.Equals(currency));
+
+        revaluatedForeignBalance += foreignBalance.CurrentBalance.Value * exchangeRate.Value;
+      }
+
+      return Math.Round(revaluatedForeignBalance, 2);
+    }
+
 
   }  // class NivelacionCuentasCompraventaVoucherBuilder
 
