ed727041911eadf0c771b51b0af9d87f9490fc4b
José Manuel Cota <jmcota@ontica.org>
2022-04-27 22:09:12 -0400

    Get currencies web api and use case added

---
+++ Core/ExchangeRates/UseCases/CurrenciesUseCases.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Exchange Rates                             Component : Use cases Layer                         *
+*  Assembly : FinancialAccounting.Core.dll               Pattern   : Use case interactor class               *
+*  Type     : CurrenciesUseCases                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases for currencies.                                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections.Generic;
+
+using Empiria.Services;
+
+namespace Empiria.FinancialAccounting.UseCases {
+
+  /// <summary>Use cases for exchange rates.</summary>
+  public class CurrenciesUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected CurrenciesUseCases() {
+      // no-op
+    }
+
+    static public CurrenciesUseCases UseCaseInteractor() {
+      return UseCase.CreateInstance<CurrenciesUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<NamedEntityDto> GetCurrencies() {
+      FixedList<Currency> currencies = Currency.GetList();
+
+      return currencies.MapToNamedEntityList();
+    }
+
+    #endregion Use cases
+
+  }  // class CurrenciesUseCases
+
+}  // namespace Empiria.FinancialAccounting.UseCases
---
+++ WebApi/ExchangeRates/CurrenciesController.cs
@@ -0,0 +1,39 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Exchange Rates                               Component : Web Api                               *
+*  Assembly : Empiria.FinancialAccounting.WebApi.dll       Pattern   : Controller                            *
+*  Type     : CurrenciesController                         License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web apis used to get information about currencies.                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.FinancialAccounting.UseCases;
+
+namespace Empiria.FinancialAccounting.WebApi {
+
+  /// <summary>Web apis used to get information about currencies.</summary>
+  public class CurrenciesController : WebApiController {
+
+    #region Web Api
+
+    [HttpGet]
+    [Route("v2/financial-accounting/currencies")]
+    public CollectionModel GetCurrencies() {
+
+      using (var usecases = CurrenciesUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> currencies = usecases.GetCurrencies();
+
+        return new CollectionModel(base.Request, currencies);
+      }
+    }
+
+    #endregion Web Api
+
+  }  // class CurrenciesController
+
+}  // namespace Empiria.FinancialAccounting.WebApi
