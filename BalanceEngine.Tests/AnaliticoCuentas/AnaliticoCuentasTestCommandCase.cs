2fa046c392c688db2663607615b773b5eb957882
José Manuel Cota <jmcota@ontica.org>
2022-05-18 23:10:26 -0400

    Read AnaliticoCuentas test expected data from local JSON files

--- BalanceEngine.Tests/AnaliticoCuentas/AnaliticoCuentasTestCommandCase.cs
+++ BalanceEngine.Tests/AnaliticoCuentas/AnaliticoCuentasTestCommandCase.cs
@@ -4,16 +4,20 @@
 *  Assembly : FinancialAccounting.BalanceEngine.Tests    Pattern   : Enumerated type with extensions         *
 *  Type     : AnaliticoCuentasTestCommandCase            License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : Test cases for Analitico de Cuentas report.                                                    *
+*  Summary  : Test command case enumeration with extensions for 'Analítico de cuentas' report.               *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+using System.IO;
+
+using Empiria.Json;
 
 using Empiria.FinancialAccounting.BalanceEngine;
 using Empiria.FinancialAccounting.BalanceEngine.Adapters;
 
 namespace Empiria.FinancialAccounting.Tests.BalanceEngine {
 
+  /// <summary>Test command case enumeration with extensions for 'Analítico de cuentas' report.</summary>
   public enum AnaliticoCuentasTestCommandCase {
 
     Default,
@@ -27,6 +31,8 @@ namespace Empiria.FinancialAccounting.Tests.BalanceEngine {
   }  // enum AnaliticoCuentasTestCommandCase
 
 
+
+  /// <summary>Extension methods for AnaliticoCuentasTestCommandCase.</summary>
   static public class AnaliticoCuentasTestCommandCaseExtensions {
 
     static public TrialBalanceCommand BuildCommand(this AnaliticoCuentasTestCommandCase commandCase) {
@@ -41,6 +47,22 @@ namespace Empiria.FinancialAccounting.Tests.BalanceEngine {
           ToDate = TestingConstants.TO_DATE
         }
       };
+    }
+
+
+    static public FixedList<AnaliticoDeCuentasEntryDto> GetExpectedEntries(this AnaliticoCuentasTestCommandCase commandCase,
+                                                                           TrialBalanceItemType filteredBy) {
+      var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+      string path = Path.Combine(directory.Parent.FullName,
+                                @"tests-data",
+                                $"analitico-cuentas.{commandCase}.test-data.json");
+
+      string jsonString = File.ReadAllText(path);
+
+      AnaliticoDeCuentasDto dto = JsonConverter.ToObject<AnaliticoDeCuentasDto>(jsonString);
+
+      return dto.Entries.FindAll(x => x.ItemType == filteredBy);
     }
 
   }  // class AnaliticoCuentasTestCommandCaseExtensions
