836f33190a9971187934a74e0c571b8d7b26457d
José Manuel Cota <jmcota@ontica.org>
2022-05-31 14:43:42 -0400

    ItemPositioning payload type added with its own validation methods

--- Core/Commons/PositioningRule.cs
+++ Core/Commons/PositioningRule.cs
@@ -29,19 +29,79 @@ namespace Empiria.FinancialAccounting {
   }  // enum PositioningRule
 
 
+  /// <summary>Payload that describes an item positioning rule to store it in an ordered list.</summary>
+  public class ItemPositioning {
 
-  /// <summary>Extension methods for PositioningRule.</summary>
+    public PositioningRule Rule {
+      get; set;
+    } = PositioningRule.Undefined;
+
+
+    public string OffsetUID {
+      get; set;
+    } = string.Empty;
+
+
+    public int Position {
+      get; set;
+    } = -1;
+
+
+    public void Clean() {
+      // no-op
+    }
+
+    public void Require() {
+      if (Rule.UsesOffset()) {
+        Assertion.Require(OffsetUID.Length != 0,
+                $"payload.Positioning.Rule is '{Rule}', so Positioning.OffsetUID must be provided.");
+      }
+
+      if (Rule.UsesPosition()) {
+        Assertion.Require(Position != -1,
+                $"payload.Positioning.Rule is '{Rule}', so Positioning.Position must be provided.");
+
+        Assertion.Require(Position > 0,
+                $"payload.PositioningRule is '{Rule}', so Positioning.Position must be greater than zero.");
+      }
+    }
+
+
+    public void SetIssues(ExecutionResult result) {
+      // no-op
+    }
+
+
+    public void SetWarnings(ExecutionResult result) {
+      result.AddWarningIf(Rule == PositioningRule.Undefined && OffsetUID.Length != 0,
+         $"payload.Positioning.Rule is '{Rule}', but Positioning.OffsetUID value was supplied.");
+
+      result.AddWarningIf(Rule == PositioningRule.Undefined && Position > 0,
+        $"payload.Positioning.Rule is '{Rule}', but Positioning.Position value was supplied.");
+
+      result.AddWarningIf(Rule.UsesOffset() && Position != -1,
+        $"payload.Positioning.Rule is '{Rule}', but Positioning.Position value was supplied.");
+
+      result.AddWarningIf(Rule.UsesPosition() && OffsetUID.Length != 0,
+        $"payload.Positioning.Rule is '{Rule}', but Positioning.OffsetUID value was supplied.");
+    }
+
+  }  // class ItemPositioning
+
+
+
+  /// <summary>Extension methods for PositioningRule enumeration.</summary>
   public static class PositioningRuleExtensions {
 
     static public bool UsesOffset(this PositioningRule rule) {
       return rule == PositioningRule.AfterOffset || rule == PositioningRule.BeforeOffset;
     }
 
-
     static public bool UsesPosition(this PositioningRule rule) {
       return rule == PositioningRule.ByPositionValue;
     }
 
   }  // class PositioningRuleExtensions
+
 
 }  // namespace Empiria.FinancialAccounting
