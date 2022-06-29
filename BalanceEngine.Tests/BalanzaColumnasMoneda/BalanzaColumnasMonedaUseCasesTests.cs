64df7c6a08918f21cbde36a92fada485e78ee6a2
efrain-jerv <efrain.rodriguez@ontica.org>
2022-06-29 16:23:06 -0700

    Use cases and webApi tests were added to BalanzaColumnasMoneda

--- BalanceEngine.Tests/BalanzaColumnasMoneda/BalanzaColumnasMonedaUseCasesTests.cs
+++ BalanceEngine.Tests/BalanzaColumnasMoneda/BalanzaColumnasMonedaUseCasesTests.cs
@@ -1,10 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Test cases                              *
+*  Assembly : FinancialAccounting.BalanceEngine.Tests    Pattern   : Use cases tests                         *
+*  Type     : BalanzaColumnasMonedaUseCasesTests         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use case tests for 'Balanza en columnas por moneda' report.                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
 using System.Threading.Tasks;
 
-namespace Empiria.FinancialAccounting.Tests.BalanzaColumnasMoneda {
-  class BalanzaColumnasMonedaUseCasesTests {
-  }
-}
+using Xunit;
+
+using Empiria.Tests;
+
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+using Empiria.FinancialAccounting.BalanceEngine.UseCases;
+
+namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaColumnasMoneda {
+
+  /// <summary>Use case tests for 'Balanza en columnas por moneda' report.</summary>
+  public class BalanzaColumnasMonedaUseCasesTests {
+
+    #region Initialization
+
+    public BalanzaColumnasMonedaUseCasesTests() {
+      TestsCommonMethods.Authenticate();
+    }
+
+    #endregion Initialization
+
+    #region Theories
+
+    [Theory]
+    [InlineData(BalanzaColumnasMonedaTestCase.Default)]
+    [InlineData(BalanzaColumnasMonedaTestCase.CatalogoAnterior)]
+    public async Task QueriesMustBeEqual(BalanzaColumnasMonedaTestCase testcase) {
+      TrialBalanceQuery query = testcase.GetInvocationQuery();
+
+      using (var usecase = TrialBalanceUseCases.UseCaseInteractor()) {
+        BalanzaColumnasMonedaDto sut = await usecase.BuildBalanzaColumnasMoneda(query);
+
+        Assert.Equal(query.GetHashCode(), sut.Query.GetHashCode());
+        Assert.Equal(query, sut.Query);
+      }
+
+    }
+
+    #endregion Theories
+
+  } // class BalanzaColumnasMonedaUseCasesTests
+
+} // namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaColumnasMoneda
--- BalanceEngine.Tests/BalanzaColumnasMoneda/BalanzaColumnasMonedaWebApiTests.cs
+++ BalanceEngine.Tests/BalanzaColumnasMoneda/BalanzaColumnasMonedaWebApiTests.cs
@@ -1,10 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Test cases                              *
+*  Assembly : FinancialAccounting.BalanceEngine.Tests    Pattern   : Web Api tests                           *
+*  Type     : BalanzaColumnasMonedaWebApiTests           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Test cases for 'Balanza en columnas por moneda' report web api calls.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
 using System.Threading.Tasks;
 
-namespace Empiria.FinancialAccounting.Tests.BalanzaColumnasMoneda {
-  class BalanzaColumnasMonedaWebApiTests {
-  }
-}
+using Xunit;
+
+using Empiria.Tests;
+using Empiria.WebApi.Client;
+
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+
+namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaColumnasMoneda {
+
+  /// <summary>Test cases for 'Balanza en columnas por moneda' report web api calls.</summary>
+  public class BalanzaColumnasMonedaWebApiTests {
+
+    #region Initialization
+
+    public BalanzaColumnasMonedaWebApiTests() {
+      TestsCommonMethods.Authenticate();
+    }
+
+    #endregion Initialization
+
+    #region Theories
+
+    [Theory]
+    [InlineData(BalanzaColumnasMonedaTestCase.Default)]
+    [InlineData(BalanzaColumnasMonedaTestCase.CatalogoAnterior)]
+    public async Task WebServiceReturnsData(BalanzaColumnasMonedaTestCase testcase) {
+      TrialBalanceQuery query = testcase.GetInvocationQuery();
+
+
+      var http = new HttpApiClient(TestingConstants.WEB_API_BASE_ADDRESS,
+                                   TimeSpan.FromSeconds(TestingConstants.WEB_API_TIMEOUT_SECONDS));
+
+      var sut = await http.PostAsync<ResponseModel<BalanzaColumnasMonedaDto>>(
+                            query, "v2/financial-accounting/balance-engine/balanza-columnas-moneda")
+                          .ConfigureAwait(false);
+
+      Assert.NotNull(sut.Data);
+      Assert.NotNull(sut.Data.Query);
+      Assert.NotNull(sut.Data.Columns);
+      Assert.NotNull(sut.Data.Entries);
+    }
+
+    #endregion Theories
+
+  } // class BalanzaColumnasMonedaWebApiTests
+
+} // namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaColumnasMoneda
