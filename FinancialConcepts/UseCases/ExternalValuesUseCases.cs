698bfc2fcbd7a74cc81181456a8ad80a9662bb37
José Manuel Cota <jmcota@ontica.org>
2022-10-27 15:30:28 -0400

    ExternalValues use cases interactor and its web api controller added

---
+++ FinancialConcepts/UseCases/ExternalValuesUseCases.cs
@@ -0,0 +1,52 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Concepts                         Component : Use cases Layer                         *
+*  Assembly : FinancialAccounting.FinancialConcepts.dll  Pattern   : Use case interactor class               *
+*  Type     : ExternalValuesUseCases                     License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases used to update and retrieve financial external values.                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Services;
+
+using Empiria.FinancialAccounting.FinancialConcepts.Adapters;
+
+namespace Empiria.FinancialAccounting.FinancialConcepts.UseCases {
+
+  /// <summary>Use cases used to update and retrieve financial external values.</summary>
+  public class ExternalValuesUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected ExternalValuesUseCases() {
+      // no-op
+    }
+
+    static public ExternalValuesUseCases UseCaseInteractor() {
+      return UseCase.CreateInstance<ExternalValuesUseCases>();
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public ExternalValuesDto GetExternalValues(ExternalValuesQuery query) {
+      Assertion.Require(query, nameof(query));
+
+      query.EnsureValid();
+
+      ExternalVariablesSet variablesSet = query.GetExternalVariablesSet();
+
+      ExternalValuesDataSet dataSet = ExternalValuesDataSet.Parse(variablesSet, query.Date);
+
+      return ExternalValuesDataSetMapper.Map(query, dataSet);
+    }
+
+    #endregion Use cases
+
+  }  // class ExternalValuesUseCases
+
+}  // namespace Empiria.FinancialAccounting.FinancialConcepts.UseCases
---
+++ WebApi/FinancialConcepts/ExternalValuesController.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Concepts                           Component : Web Api                               *
+*  Assembly : Empiria.FinancialAccounting.WebApi.dll       Pattern   : Query Controller                      *
+*  Type     : ExternalValuesController                     License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used to retrive and upload external variables values.                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.FinancialAccounting.FinancialConcepts.Adapters;
+using Empiria.FinancialAccounting.FinancialConcepts.UseCases;
+
+namespace Empiria.FinancialAccounting.WebApi.FinancialConcepts {
+
+  /// <summary>Web API used to retrive and upload external variables.</summary>
+  public class ExternalValuesController : WebApiController {
+
+    #region Web Apis
+
+    [HttpPost]
+    [Route("v2/financial-accounting/financial-concepts/external-values")]
+    public SingleObjectModel GetExternalValues([FromBody] ExternalValuesQuery query) {
+
+      base.RequireBody(query);
+
+      using (var usecases = ExternalValuesUseCases.UseCaseInteractor()) {
+        ExternalValuesDto result = usecases.GetExternalValues(query);
+
+        return new SingleObjectModel(base.Request, result);
+      }
+    }
+
+    #endregion Web Apis
+
+  }  // class ExternalValuesController
+
+}  // namespace Empiria.FinancialAccounting.WebApi.FinancialConcepts
