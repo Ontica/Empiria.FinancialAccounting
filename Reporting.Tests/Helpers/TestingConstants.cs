3e5e3f525bb14bc2c5c7cff1666ffe0fda115fc0
José Manuel Cota <jmcota@ontica.org>
2022-11-04 23:04:59 -0400

    Reporting services tests module added

---
+++ Reporting.Tests/Helpers/TestingConstants.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Reporting Services                           Component : Test cases                            *
+*  Assembly : FinancialAccounting.Reporting.Tests          Pattern   : Testing constants                     *
+*  Type     : TestingConstants                             License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides testing constants.                                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.FinancialAccounting.Tests {
+
+  /// <summary>Provides testing constants.</summary>
+  static public class TestingConstants {
+
+  }  // class TestingConstants
+
+}  // namespace Empiria.FinancialAccounting.Tests
