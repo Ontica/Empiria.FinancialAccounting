b7707af22f99edfc630c574edf205942f1f684ee
efrain-jerv <efrain.rodriguez@ontica.org>
2022-11-08 09:44:06 -0700

    Test for Comparativo de cuentas was added to Reporting.Tests

---
+++ Reporting.Tests/ReportesOperativos/ComparativoDeCuentasTests.cs
@@ -0,0 +1,46 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Test cases                              *
+*  Assembly : FinancialAccounting.BalanceEngine.Tests    Pattern   : Use cases tests                         *
+*  Type     : ComparativoDeCuentasTests                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Test cases for Comparativo de cuentas.                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Empiria.FinancialAccounting.Reporting;
+using Xunit;
+
+namespace Empiria.FinancialAccounting.Tests.Reporting {
+
+  /// <summary>Test cases for Comparativo de cuentas.</summary>
+  public class ComparativoDeCuentasTests {
+
+
+    [Fact]
+    public void Should_Build_Comparativo_De_Cuentas() {
+
+      ReportBuilderQuery query = new ReportBuilderQuery {
+        AccountsChartUID = "47ec2ec7-0f4f-482e-9799-c23107b60d8a",
+        AccountNumber = "3.02.01",
+        OutputType = "a99756ce-2c80-4680-ae20-c897616fe155",
+        ReportType = ReportTypes.ComparativoDeCuentas,
+        FromDate = new DateTime(2022, 06, 01),
+        ToDate = new DateTime(2022, 06, 30)
+      };
+
+      using (var service = ReportingService.ServiceInteractor()) {
+        ReportDataDto sut = service.GenerateReport(query);
+
+        Assert.NotNull(sut);
+        Assert.Equal(query, sut.Query);
+        Assert.NotEmpty(sut.Entries);
+      }
+
+
+    }
+
+
+  } // class ComparativoDeCuentasTests
+
+} // 
