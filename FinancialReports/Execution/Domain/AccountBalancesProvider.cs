292237f5b87db926c266f2b4a8606f6ac52938ac
José Manuel Cota <jmcota@ontica.org>
2022-12-04 11:03:24 -0500

    Rfx Encapsulate balances hash table into AccountBalancesProvider

--- FinancialReports/Execution/Domain/AccountBalancesProvider.cs
+++ FinancialReports/Execution/Domain/AccountBalancesProvider.cs
@@ -12,8 +12,11 @@ using System.Linq;
 
 using Empiria.Collections;
 
+using Empiria.FinancialAccounting.BalanceEngine;
 using Empiria.FinancialAccounting.BalanceEngine.Adapters;
 using Empiria.FinancialAccounting.BalanceEngine.UseCases;
+
+using Empiria.FinancialAccounting.FinancialConcepts;
 using Empiria.FinancialAccounting.FinancialReports.Adapters;
 
 namespace Empiria.FinancialAccounting.FinancialReports {
@@ -22,6 +25,9 @@ namespace Empiria.FinancialAccounting.FinancialReports {
   internal class AccountBalancesProvider {
 
     private readonly FinancialReportQuery _buildQuery;
+    private readonly FinancialReportType _financialReportType;
+
+    private readonly EmpiriaHashTable<FixedList<ITrialBalanceEntryDto>> _balances;
 
     #region Constructors and parsers
 
@@ -29,19 +35,110 @@ namespace Empiria.FinancialAccounting.FinancialReports {
       Assertion.Require(buildQuery, nameof(buildQuery));
 
       _buildQuery = buildQuery;
+      _financialReportType = _buildQuery.GetFinancialReportType();
+      _balances = GetBalancesAsHashTable();
     }
 
     #endregion Constructors and parsers
 
     #region Public methods
 
-    internal EmpiriaHashTable<FixedList<ITrialBalanceEntryDto>> GetBalancesAsHashTable() {
-      var balances = GetBalances();
+    public bool ContainsAccount(string accountNumber) {
+      return _balances.ContainsKey(accountNumber);
+    }
+
+
+    public FixedList<ITrialBalanceEntryDto> GetAccountBalances(FinancialConceptEntry integrationEntry) {
+      FixedList<ITrialBalanceEntryDto> balances = _balances[integrationEntry.AccountNumber];
+
+      FixedList<ITrialBalanceEntryDto> filtered;
+
+      if (integrationEntry.HasSector && integrationEntry.HasSubledgerAccount) {
+        filtered = balances.FindAll(x => x.SectorCode == integrationEntry.SectorCode &&
+                                         x.SubledgerAccountNumber == integrationEntry.SubledgerAccountNumber);
+
+      } else if (integrationEntry.HasSector && !integrationEntry.HasSubledgerAccount) {
+        filtered = balances.FindAll(x => x.SectorCode == integrationEntry.SectorCode &&
+                                         x.SubledgerAccountNumber.Length <= 1);
+
+      } else if (!integrationEntry.HasSector && integrationEntry.HasSubledgerAccount) {
+        filtered = balances.FindAll(x => x.SectorCode == "00" &&
+                                         x.SubledgerAccountNumber == integrationEntry.SubledgerAccountNumber);
+        if (filtered.Count == 0) {
+          filtered = balances.FindAll(x => x.SectorCode != "00" &&
+                                           x.SubledgerAccountNumber == integrationEntry.SubledgerAccountNumber);
+        }
+      } else {
+        filtered = balances.FindAll(x => x.SectorCode == "00" &&
+                                         x.SubledgerAccountNumber.Length <= 1);
+        if (filtered.Count == 0) {
+          filtered = balances.FindAll(x => x.SectorCode != "00" &&
+                                           x.SubledgerAccountNumber.Length <= 1);
+        }
+      }
+
+      if (_financialReportType.DataSource == FinancialReportDataSource.AnaliticoCuentas ||
+          _financialReportType.DataSource == FinancialReportDataSource.BalanzaTradicional) {
+
+        return ConvertToDynamicTrialBalanceEntryDto(filtered);
+
+      } else {
+
+        return filtered;
+
+      }
+    }
+
+
+    private FixedList<ITrialBalanceEntryDto> ConvertToDynamicTrialBalanceEntryDto(FixedList<ITrialBalanceEntryDto> sourceEntries) {
+      var converter = new DynamicTrialBalanceEntryConverter();
+
+      FixedList<DynamicTrialBalanceEntryDto> convertedEntries = converter.Convert(sourceEntries);
+
+      return convertedEntries.Select(entry => (ITrialBalanceEntryDto) entry)
+                             .ToFixedList();
+    }
+
+    #endregion Public methods
+
+    #region Helper methods
+
+    private TrialBalanceQuery DetermineTrialBalanceQuery() {
+
+      switch (_financialReportType.DataSource) {
+
+        case FinancialReportDataSource.AnaliticoCuentas:
+          return GetAnaliticoCuentasQuery();
+
+        case FinancialReportDataSource.BalanzaEnColumnasPorMoneda:
+          return GetBalanzaEnColumnasPorMonedaQuery();
+
+        case FinancialReportDataSource.BalanzaTradicional:
+          return GetBalanzaTradicionalQuery();
+
+        default:
+          throw Assertion.EnsureNoReachThisCode(
+              $"Unrecognized balances source {_financialReportType.DataSource} for report type {_financialReportType.Name}.");
+      }
+    }
+
+
+    private TrialBalanceDto InvokeGetBalancesUseCase() {
+      TrialBalanceQuery query = DetermineTrialBalanceQuery();
+
+      using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
+        return usecases.BuildTrialBalance(query);
+      }
+    }
+
+
+    private EmpiriaHashTable<FixedList<ITrialBalanceEntryDto>> GetBalancesAsHashTable() {
+      var balances = InvokeGetBalancesUseCase();
 
       var converted = new
                 FixedList<ITrialBalanceEntryDto>(balances.Entries.FindAll(x =>
-                                                 x.ItemType == BalanceEngine.TrialBalanceItemType.Entry ||
-                                                 x.ItemType == BalanceEngine.TrialBalanceItemType.Summary));
+                                                 x.ItemType == TrialBalanceItemType.Entry ||
+                                                 x.ItemType == TrialBalanceItemType.Summary));
 
 
       var distinctAccountNumbersList = converted.Select(x => x.AccountNumber)
@@ -57,48 +154,14 @@ namespace Empiria.FinancialAccounting.FinancialReports {
       return hashTable;
     }
 
-    #endregion Public methods
-
-    #region Helper methods
-
-    private TrialBalanceQuery DetermineTrialBalanceQuery() {
-      FinancialReportType reportType = _buildQuery.GetFinancialReportType();
-
-      switch (reportType.DataSource) {
-
-        case FinancialReportDataSource.AnaliticoCuentas:
-          return GetAnaliticoCuentasQuery();
-
-        case FinancialReportDataSource.BalanzaEnColumnasPorMoneda:
-          return GetBalanzaEnColumnasPorMonedaQuery();
-
-        case FinancialReportDataSource.BalanzaTradicional:
-          return GetBalanzaTradicionalQuery();
-
-        default:
-          throw Assertion.EnsureNoReachThisCode(
-              $"Unrecognized balances source {reportType.DataSource} for report type {reportType.Name}.");
-      }
-    }
-
-
-    private TrialBalanceDto GetBalances() {
-      TrialBalanceQuery query = DetermineTrialBalanceQuery();
-
-      using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
-        return usecases.BuildTrialBalance(query);
-      }
-    }
-
-
     private TrialBalanceQuery GetAnaliticoCuentasQuery() {
       return new TrialBalanceQuery {
         AccountsChartUID = _buildQuery.AccountsChartUID,
-        TrialBalanceType = BalanceEngine.TrialBalanceType.AnaliticoDeCuentas,
+        TrialBalanceType = TrialBalanceType.AnaliticoDeCuentas,
         UseDefaultValuation = true,
         ShowCascadeBalances = false,
         WithSubledgerAccount = false,  // true
-        BalancesType = BalanceEngine.BalancesType.WithCurrentBalanceOrMovements,
+        BalancesType = BalancesType.WithCurrentBalanceOrMovements,
         ConsolidateBalancesToTargetCurrency = false,
         InitialPeriod = new BalancesPeriod {
           FromDate = new DateTime(_buildQuery.ToDate.Year, _buildQuery.ToDate.Month, 1),
@@ -112,11 +175,11 @@ namespace Empiria.FinancialAccounting.FinancialReports {
     private TrialBalanceQuery GetBalanzaEnColumnasPorMonedaQuery() {
       return new TrialBalanceQuery {
         AccountsChartUID = _buildQuery.AccountsChartUID,
-        TrialBalanceType = BalanceEngine.TrialBalanceType.BalanzaEnColumnasPorMoneda,
+        TrialBalanceType = TrialBalanceType.BalanzaEnColumnasPorMoneda,
         UseDefaultValuation = true,
         ShowCascadeBalances = false,
         WithSubledgerAccount = false,
-        BalancesType = BalanceEngine.BalancesType.WithCurrentBalanceOrMovements,
+        BalancesType = BalancesType.WithCurrentBalanceOrMovements,
         ConsolidateBalancesToTargetCurrency = false,
         InitialPeriod = new BalancesPeriod {
           FromDate = new DateTime(_buildQuery.ToDate.Year, _buildQuery.ToDate.Month, 1),
@@ -130,11 +193,11 @@ namespace Empiria.FinancialAccounting.FinancialReports {
     private TrialBalanceQuery GetBalanzaTradicionalQuery() {
       return new TrialBalanceQuery {
         AccountsChartUID = _buildQuery.AccountsChartUID,
-        TrialBalanceType = BalanceEngine.TrialBalanceType.Balanza,
+        TrialBalanceType = TrialBalanceType.Balanza,
         ShowCascadeBalances = false,
         WithSubledgerAccount = false,
         UseDefaultValuation = false,
-        BalancesType = BalanceEngine.BalancesType.WithCurrentBalanceOrMovements,
+        BalancesType = BalancesType.WithCurrentBalanceOrMovements,
         ConsolidateBalancesToTargetCurrency = true,
         InitialPeriod = new BalancesPeriod {
           FromDate = new DateTime(_buildQuery.ToDate.Year, _buildQuery.ToDate.Month, 1),
