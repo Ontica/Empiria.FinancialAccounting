9d87c15f804c9ad78223c1fe04d7823a302018e1
José Manuel Cota <jmcota@ontica.org>
2022-12-29 21:34:10 -0500

    Special voucher builder 'Cancelación de cuentas de resultados' was added

--- Vouchers/Domain/SpecialCases/CancelacionCuentasResultadosVoucherBuilder.cs
+++ Vouchers/Domain/SpecialCases/CancelacionCuentasResultadosVoucherBuilder.cs
@@ -11,14 +11,16 @@
 using System;
 using System.Collections.Generic;
 
+using Empiria.FinancialAccounting.BalanceEngine;
 using Empiria.FinancialAccounting.BalanceEngine.Adapters;
 using Empiria.FinancialAccounting.BalanceEngine.UseCases;
+
 using Empiria.FinancialAccounting.Vouchers.Adapters;
 
 namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
 
   /// <summary>Builds a voucher that cancels the balances of profit and loss
-  /// accounts (cuentas de resultados) at a given date.</summary>
+  /// accounts at a given date (cuentas de resultados).</summary>
   internal class CancelacionCuentasResultadosVoucherBuilder : VoucherBuilder {
 
     internal CancelacionCuentasResultadosVoucherBuilder() {
@@ -38,7 +40,7 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
       FixedList<string> issues = this.ImplementsDryRun(entries);
 
       Assertion.Require(issues.Count == 0,
-        "There were one or more issues generating 'Cancelación de cuentas de resultados' voucher: " +
+        $"There were one or more issues generating '{base.SpecialCaseType.Name}' voucher: " +
         EmpiriaString.ToString(issues));
 
       var voucher = new Voucher(base.Fields);
@@ -56,9 +58,7 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
 
         entryFields.VoucherId = voucher.Id;
 
-        VoucherEntry voucherEntry = voucher.AppendEntry(entryFields);
-
-        voucherEntry.Save();
+        voucher.AppendAndSaveEntry(entryFields);
       }
     }
 
@@ -71,11 +71,12 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
     }
 
 
-    private TrialBalanceQuery BuildBalancesQuery() {
-      return new TrialBalanceQuery {
-        TrialBalanceType = BalanceEngine.TrialBalanceType.Balanza,
+    private FixedList<TrialBalanceEntryDto> GetBalances() {
+      var query = new TrialBalanceQuery {
+        TrialBalanceType = TrialBalanceType.GeneracionDeSaldos,
         AccountsChartUID = base.Fields.AccountsChartUID,
-        BalancesType = BalanceEngine.BalancesType.WithCurrentBalanceOrMovements,
+        Ledgers = new[] { base.Fields.LedgerUID },
+        BalancesType = BalancesType.WithCurrentBalance,
         ShowCascadeBalances = true,
         WithSubledgerAccount = true,
         InitialPeriod = new BalancesPeriod {
@@ -83,47 +84,129 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
           ToDate = base.Fields.CalculationDate
         }
       };
-    }
 
-
-    private FixedList<BalanzaTradicionalEntryDto> BuildTrialBalanceEntries() {
       using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
-        TrialBalanceQuery query = BuildBalancesQuery();
-
         var entries = usecases.BuildTrialBalance(query)
                               .Entries;
 
-        return entries.Select(x => (BalanzaTradicionalEntryDto) x)
+        return entries.Select(x => (TrialBalanceEntryDto) x)
                       .ToFixedList();
       }
     }
 
 
     private FixedList<VoucherEntryFields> BuildVoucherEntries() {
-      FixedList<AccountsListItem> accountsListItems = GetCancelacionResultadosAccountsList();
+      FixedList<AccountsListItem> cancelationRulesList = base.SpecialCaseType.AccountsList.GetItems();
 
-      FixedList<BalanzaTradicionalEntryDto> balances = BuildTrialBalanceEntries();
+      FixedList<TrialBalanceEntryDto> balances = GetBalances();
 
-      var entries = new List<VoucherEntryFields>();
+      var voucherEntries = new List<VoucherEntryFields>();
 
-      foreach (var item in accountsListItems) {
-        // Replace this: For each item in accountsListItems, GET last level TrialBalancesEntries list with subledger accounts and sectors
-        decimal registeredBalance = GetBalance(item.AccountNumber, balances);
+      foreach (var cancelationRule in cancelationRulesList) {
+        FixedList<TrialBalanceEntryDto> accountsToCancelBalances = balances.FindAll(x => x.AccountNumberForBalances.StartsWith(cancelationRule.AccountNumber));
 
-        // for each balance entry, build the voucher entries
-        if (registeredBalance != 0) {
-          entries.AddRange(BuildVoucherEntryFieldsList(item, registeredBalance));
+        var ruleVoucherEntries = new List<VoucherEntryFields>();
+
+        foreach (var accountBalance in accountsToCancelBalances) {
+          VoucherEntryFields voucherEntry = BuildVoucherEntry(accountBalance);
+
+          if (voucherEntry != null) {     /// ToDo - WARNING: Remove this IF after fix locked balances. No nulls must be returned
+            ruleVoucherEntries.Add(voucherEntry);
+          }
+        }
+
+        FixedList<VoucherEntryFields> targetAccountVoucherEntries = BuildTargetAccountVoucherEntry(ruleVoucherEntries,
+                                                                                                   cancelationRule.TargetAccountNumber);
+
+        ruleVoucherEntries.AddRange(targetAccountVoucherEntries);
+
+        voucherEntries.AddRange(ruleVoucherEntries);
+      }
+
+      Assertion.Require(voucherEntries.Count >= 2,
+                        "No hay saldos de cuentas de resultados por cancelar a la fecha proporcionada.");
+
+      return voucherEntries.ToFixedList();
+    }
+
+
+    private FixedList<VoucherEntryFields> BuildTargetAccountVoucherEntry(List<VoucherEntryFields> accumulatedEntries,
+                                                                         string targetAccountNumber) {
+      decimal totalDebits = 0m;
+      decimal totalCredits = 0m;
+
+      foreach (var entry in accumulatedEntries) {
+        if (entry.VoucherEntryType == VoucherEntryType.Debit) {
+          totalDebits += entry.Amount;
+        } else {
+          totalCredits += entry.Amount;
         }
       }
 
-      Assertion.Require(entries.Count >= 2,
-            "No hay saldos de cuentas de resultados por cancelar a la fecha proporcionada.");
+      var entries = new List<VoucherEntryFields>();
+
+      if (totalDebits != 0) {
+        entries.Add(BuildVoucherEntryFields(VoucherEntryType.Credit, targetAccountNumber,
+                                            "00", SubledgerAccount.Empty, totalDebits));
+      }
+      if (totalCredits != 0) {
+        entries.Add(BuildVoucherEntryFields(VoucherEntryType.Debit, targetAccountNumber,
+                                            "00", SubledgerAccount.Empty, totalCredits));
+      }
 
       return entries.ToFixedList();
     }
 
 
-    private VoucherEntryFields BuildVoucherEntryFields(string accountNumber, decimal amount, bool isDebit) {
+    private VoucherEntryFields BuildVoucherEntry(TrialBalanceEntryDto accountBalance) {
+
+      if (accountBalance.AccountNumberForBalances == "6.05.01.02.03.03" &&     // ToDo - WARNING: Remove this code after fix locked balances
+          accountBalance.SubledgerAccountId <= 0) {
+        return null;
+      }
+
+      if (accountBalance.DebtorCreditor == "Deudora" &&
+          accountBalance.CurrentBalance > 0) {
+        return BuildVoucherEntryFields(VoucherEntryType.Credit, accountBalance);
+
+      } else if (accountBalance.DebtorCreditor == "Deudora" &&
+                 accountBalance.CurrentBalance < 0) {
+
+        return BuildVoucherEntryFields(VoucherEntryType.Debit, accountBalance);
+
+      } else if (accountBalance.DebtorCreditor == "Acreedora" &&
+                 accountBalance.CurrentBalance > 0) {
+        return BuildVoucherEntryFields(VoucherEntryType.Debit, accountBalance);
+
+      } else if (accountBalance.DebtorCreditor == "Acreedora" &&
+                 accountBalance.CurrentBalance < 0) {
+        return BuildVoucherEntryFields(VoucherEntryType.Credit, accountBalance);
+      }
+
+      throw Assertion.EnsureNoReachThisCode();
+    }
+
+
+    private VoucherEntryFields BuildVoucherEntryFields(VoucherEntryType entryType,
+                                                       TrialBalanceEntryDto accountBalance) {
+      // ToDo: Must Parse the subledger account using the id because subledgerNumbers are coming empty from the balance engine.
+      var subledgerAccount = SubledgerAccount.Parse(accountBalance.SubledgerAccountId);
+
+      return BuildVoucherEntryFields(entryType,
+                                     accountBalance.AccountNumberForBalances,
+                                     accountBalance.SectorCode,
+                                     subledgerAccount,
+                                     Math.Abs(accountBalance.CurrentBalance.Value)
+                                    );
+    }
+
+
+    private VoucherEntryFields BuildVoucherEntryFields(VoucherEntryType entryType,
+                                                   string accountNumber,
+                                                   string sectorCode,
+                                                   SubledgerAccount subledgerAccount,
+                                                   decimal balance) {
+
       StandardAccount stdAccount = AccountsChart.Parse(base.Fields.AccountsChartUID)
                                                 .GetStandardAccount(accountNumber);
 
@@ -132,50 +215,19 @@ namespace Empiria.FinancialAccounting.Vouchers.SpecialCases {
       LedgerAccount ledgerAccount = ledger.AssignAccount(stdAccount);
 
       return new VoucherEntryFields {
-        Amount = amount,
-        BaseCurrencyAmount = amount,
+        Amount = balance,
+        BaseCurrencyAmount = balance,
         CurrencyUID = ledger.BaseCurrency.UID,
-        SectorId = Sector.Empty.Id,
-        SubledgerAccountNumber = String.Empty,
+        SectorId = Sector.Parse(sectorCode).Id,
+        SubledgerAccountId = subledgerAccount.Id,
+        SubledgerAccountNumber = subledgerAccount.IsEmptyInstance ?
+                                                    string.Empty : subledgerAccount.Number,
         StandardAccountId = stdAccount.Id,
         LedgerAccountId = ledgerAccount.Id,
-        VoucherEntryType = isDebit ? VoucherEntryType.Debit : VoucherEntryType.Credit
+        VoucherEntryType = entryType
       };
     }
 
-
-    private List<VoucherEntryFields> BuildVoucherEntryFieldsList(AccountsListItem item,
-                                                                 decimal registeredBalance) {
-      var entries = new List<VoucherEntryFields>();
-
-      decimal amount = registeredBalance;
-
-      if (amount > 0) {     // Check if Debtor || Creditor. Create an entry for each subledger account and/or sector if applies
-        entries.Add(BuildVoucherEntryFields(item.AccountNumber, amount, true));
-        entries.Add(BuildVoucherEntryFields(item.TargetAccountNumber, amount, false));
-      } else if (amount < 0) {
-        entries.Add(BuildVoucherEntryFields(item.AccountNumber, Math.Abs(amount), false));
-        entries.Add(BuildVoucherEntryFields(item.TargetAccountNumber, Math.Abs(amount), true));
-      }
-
-      return entries;
-    }
-
-
-    private decimal GetBalance(string accountNumber, FixedList<BalanzaTradicionalEntryDto> balances) {
-      var balance = balances.Find(x => x.AccountNumber == accountNumber &&
-                                       x.LedgerUID == base.Fields.LedgerUID);
-
-      if (balance == null) {
-        return 0;
-      }
-      return balance.CurrentBalance.Value;
-    }
-
-
-    private FixedList<AccountsListItem> GetCancelacionResultadosAccountsList() {
-      return base.SpecialCaseType.AccountsList.GetItems();
-    }
 
   }  // class CancelacionCuentasResultadosVoucherBuilder
 
