93abd809c3c763d7f776aa767cd08a2997795c8e
José Manuel Cota <jmcota@ontica.org>
2023-03-07 10:50:24 -0500

    Rfx R04 related function handlers were replaced by code macros

--- FinancialReports/Execution/Providers/FinancialFunctionsLibrary.cs
+++ FinancialReports/Execution/Providers/FinancialFunctionsLibrary.cs
@@ -31,18 +31,6 @@ namespace Empiria.FinancialAccounting.FinancialReports.Providers {
     private void LoadFunctions() {
       var functions = new[] {
 
-        new Function(lexeme: "CALCULAR_ABONOS_R04", arity: 0,
-                     calle: () => new CalcularAbonosR04()),
-
-        new Function(lexeme: "CALCULAR_ABONOS_EFECTO_VALUACION_R04", arity: 0,
-                     calle: () => new CalcularAbonosEfectoValuacionR04()),
-
-        new Function(lexeme: "CALCULAR_CARGOS_R04", arity: 0,
-                     calle: () => new CalcularCargosR04()),
-
-        new Function(lexeme: "CALCULAR_CARGOS_EFECTO_VALUACION_R04", arity: 0,
-                     calle: () => new CalcularCargosEfectoValuacionR04()),
-
         new Function(lexeme: "DEUDORAS_MENOS_ACREEDORAS", arity: 3,
                      calle: () => new DeudorasMenosAcreedorasFunction()),
 
@@ -54,88 +42,6 @@ namespace Empiria.FinancialAccounting.FinancialReports.Providers {
       base.AddRange(functions);
     }
 
-
-    sealed private class CalcularAbonosR04 : FunctionHandler {
-
-      protected override object Evaluate() {
-        IFinancialConceptValues _this = (IFinancialConceptValues) base.Data["this"];
-
-        decimal abonosPesos = _this.GetTotalField("abonosPesos");
-        decimal abonosMonExt = _this.GetTotalField("abonosMonExtVal");
-        decimal abonosUDIs = _this.GetTotalField("abonosUDIsVal");
-
-        _this.SetTotalField("totalMonedaNacional", abonosPesos);
-        _this.SetTotalField("totalMonedaExtranjera", abonosMonExt);
-        _this.SetTotalField("totalUdis", abonosUDIs);
-
-        _this.SetTotalField("totalR04", abonosPesos + abonosMonExt + abonosUDIs);
-
-        return _this;
-      }
-
-    }  // CalcularAbonosR04
-
-
-    sealed private class CalcularAbonosEfectoValuacionR04 : FunctionHandler {
-
-      protected override object Evaluate() {
-        IFinancialConceptValues _this = (IFinancialConceptValues) base.Data["this"];
-
-        decimal abonosMonExt = _this.GetTotalField("abonosEfectosValuacionMonExt");
-        decimal abonosUDIs = _this.GetTotalField("abonosEfectosValuacionUDIs");
-
-        _this.SetTotalField("totalMonedaNacional", 0);
-        _this.SetTotalField("totalMonedaExtranjera", abonosMonExt);
-        _this.SetTotalField("totalUdis", abonosUDIs);
-
-        _this.SetTotalField("totalR04", abonosMonExt + abonosUDIs);
-
-        return _this;
-      }
-
-    }  // class CalcularAbonosEfectoValuacionR04
-
-
-    sealed private class CalcularCargosR04 : FunctionHandler {
-
-      protected override object Evaluate() {
-        IFinancialConceptValues _this = (IFinancialConceptValues) base.Data["this"];
-
-        decimal cargosPesos = _this.GetTotalField("cargosPesos");
-        decimal cargosMonExt = _this.GetTotalField("cargosMonExtVal");
-        decimal cargosUDIs = _this.GetTotalField("cargosUDIsVal");
-
-        _this.SetTotalField("totalMonedaNacional", cargosPesos);
-        _this.SetTotalField("totalMonedaExtranjera", cargosMonExt);
-        _this.SetTotalField("totalUdis", cargosUDIs);
-
-        _this.SetTotalField("totalR04", cargosPesos + cargosMonExt + cargosUDIs);
-
-        return _this;
-      }
-
-    }  // class CalcularCargosR04
-
-
-    sealed private class CalcularCargosEfectoValuacionR04 : FunctionHandler {
-
-      protected override object Evaluate() {
-        IFinancialConceptValues _this = (IFinancialConceptValues) base.Data["this"];
-
-        decimal cargosMonExt = _this.GetTotalField("cargosEfectosValuacionMonExt");
-        decimal cargosUDIs = _this.GetTotalField("cargosEfectosValuacionUDIs");
-
-        _this.SetTotalField("totalMonedaNacional", 0);
-        _this.SetTotalField("totalMonedaExtranjera", cargosMonExt);
-        _this.SetTotalField("totalUdis", cargosUDIs);
-
-        _this.SetTotalField("totalR04", cargosMonExt + cargosUDIs);
-        return _this;
-      }
-
-    }  //  class CalcularCargosEfectoValuacionR04
-
-
     /// <summary>Returns deudoras minus acreedoras, or acreedoras minus deudoras balance,
     /// depending on the concept code.</summary>
     sealed private class DeudorasMenosAcreedorasFunction : FunctionHandler {
@@ -145,11 +51,6 @@ namespace Empiria.FinancialAccounting.FinancialReports.Providers {
         FinancialConcept concept = GetObject<FinancialConcept>(Parameters[0]);
         decimal deudorasBalance = GetDecimal(Parameters[1]);
         decimal acreedorasBalance = GetDecimal(Parameters[2]);
-
-        //if (!concept.Group.Tags.Contains("DeudorasMenosAcreedoras")) {
-
-        //  return deudorasBalance - acreedorasBalance;
-        //}
 
         if (concept.Code.Contains(",")) {
 
@@ -186,7 +87,7 @@ namespace Empiria.FinancialAccounting.FinancialReports.Providers {
         return _conceptsCalculator.Calculate(financialConcept);
       }
 
-    }  // DeudorasMenosAcreedorasFunction
+    }  // ValoresConceptoFunction
 
   }  // class FinancialFunctionsLibrary
 
