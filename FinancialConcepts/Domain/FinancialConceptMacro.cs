5d3bd1a95ac5cd3688e3be2f4c2c648d9678662e
José Manuel Cota <jmcota@ontica.org>
2023-03-07 08:46:12 -0500

    FinancialConceptMacro type that handles user defined code was added

---
+++ FinancialConcepts/Domain/FinancialConceptMacro.cs
@@ -0,0 +1,47 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Concepts                         Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.FinancialConcepts.dll  Pattern   : Service provider                        *
+*  Type     : FinancialConceptMacro                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Programming macros that contain user defined code for financial concepts calculation.          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Expressions;
+
+namespace Empiria.FinancialAccounting.FinancialConcepts {
+
+  /// <summary>Programming macros that contain user defined code for financial concepts calculation.</summary>
+  public class FinancialConceptMacro : BaseObject, IMacro {
+
+    #region Constructors and parsers
+
+    static public FixedList<FinancialConceptMacro> GetList() {
+      return BaseObject.GetList<FinancialConceptMacro>()
+                       .ToFixedList();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    [DataField("ObjectName")]
+    public string Name {
+      get;
+      private set;
+    }
+
+
+    [DataField("ObjectExtData")]
+    public string Code {
+      get;
+      private set;
+    }
+
+    #endregion Properties
+
+  }  // class FinancialConceptMacro
+
+}  // namespace Empiria.FinancialAccounting.FinancialConcepts
