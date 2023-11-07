ae2f31a48850843e8ea9927baa0c3cc999a356ad
José Manuel Cota <jmcota@ontica.org>
2023-11-07 14:08:46 -0600

    Sectors web api and use cases added

---
+++ Core/Catalogues/UseCases/SectorsUseCases.cs
@@ -0,0 +1,43 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Catalogues                                 Component : Use cases Layer                         *
+*  Assembly : FinancialAccounting.Core.dll               Pattern   : Use case interactor class               *
+*  Type     : SectorsUseCases                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases for sectors.                                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Services;
+
+namespace Empiria.FinancialAccounting.UseCases {
+
+  /// <summary>Use cases for sectors.</summary>
+  public class SectorsUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected SectorsUseCases() {
+      // no-op
+    }
+
+    static public SectorsUseCases UseCaseInteractor() {
+      return UseCase.CreateInstance<SectorsUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<NamedEntityDto> GetSectors() {
+      FixedList<Sector> sectors = Sector.GetList();
+
+      return sectors.MapToNamedEntityList();
+    }
+
+    #endregion Use cases
+
+  }  // class SectorsUseCases
+
+}  // namespace Empiria.FinancialAccounting.UseCases
---
+++ WebApi/Catalogues/SectorsController.cs
@@ -0,0 +1,39 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Catalogues                                   Component : Web Api                               *
+*  Assembly : Empiria.FinancialAccounting.WebApi.dll       Pattern   : Controller                            *
+*  Type     : SectorsController                            License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web apis used to get information about sectors.                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.FinancialAccounting.UseCases;
+
+namespace Empiria.FinancialAccounting.WebApi {
+
+  /// <summary>Web apis used to get information about sectors.</summary>
+  public class SectorsController : WebApiController {
+
+    #region Web Api
+
+    [HttpGet]
+    [Route("v2/financial-accounting/catalogues/sectors")]
+    public CollectionModel GetSectors() {
+
+      using (var usecases = SectorsUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> sectors = usecases.GetSectors();
+
+        return new CollectionModel(base.Request, sectors);
+      }
+    }
+
+    #endregion Web Api
+
+  }  // class SectorsController
+
+}  // namespace Empiria.FinancialAccounting.WebApi
