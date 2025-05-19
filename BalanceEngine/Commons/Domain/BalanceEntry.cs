2f3975e97c2d8e61f4d2887df26e4f82c7ef2408
efrain-jerv <efrain.rodriguez@ontica.org>
2025-05-19 11:40:47 -0700

    BalanceEntry object class added for balance testing at movement level

---
+++ BalanceEngine/Commons/Domain/BalanceEntry.cs
@@ -0,0 +1,107 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Empiria Plain Object                    *
+*  Type     : TrialBalanceEntry                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an entry for a balance entry.                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Represents an entry for a balance entry.</summary>
+  public class BalanceEntry {
+
+    #region Constructors and parsers
+
+    internal BalanceEntry() {
+      // Required by Empiria Framework.
+    }
+
+    #endregion Constructors and parsers
+
+    public TrialBalanceItemType ItemType {
+      get;
+      internal set;
+    } = TrialBalanceItemType.Entry;
+
+
+    [DataField("ID_MAYOR", ConvertFrom = typeof(decimal))]
+    public Ledger Ledger {
+      get;
+      internal set;
+    }
+
+    [DataField("ID_MONEDA", ConvertFrom = typeof(decimal))]
+    public Currency Currency {
+      get;
+      internal set;
+    }
+
+
+    [DataField("ID_CUENTA_ESTANDAR", ConvertFrom = typeof(long))]
+    public StandardAccount Account {
+      get;
+      internal set;
+    }
+
+
+    [DataField("ID_SECTOR", ConvertFrom = typeof(long))]
+    public Sector Sector {
+      get;
+      internal set;
+    }
+
+
+    [DataField("ID_CUENTA_AUXILIAR", ConvertFrom = typeof(decimal))]
+    public int SubledgerAccountId {
+      get;
+      internal set;
+    }
+
+
+    [DataField("SALDO_ANTERIOR")]
+    public decimal InitialBalance {
+      get;
+      internal set;
+    }
+
+
+    [DataField("DEBE")]
+    public decimal Debit {
+      get;
+      internal set;
+    }
+
+
+    [DataField("HABER")]
+    public decimal Credit {
+      get;
+      internal set;
+    }
+
+
+    [DataField("SALDO_ACTUAL")]
+    public decimal CurrentBalance {
+      get;
+      internal set;
+    }
+
+
+    [DataField("SALDO_PROMEDIO")]
+    public decimal AverageBalance {
+      get;
+      internal set;
+    }
+
+
+    [DataField("FECHA_ULTIMO_MOVIMIENTO")]
+    public DateTime LastChangeDate {
+      get; internal set;
+    }
+
+  } //class BalanceEntry
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
