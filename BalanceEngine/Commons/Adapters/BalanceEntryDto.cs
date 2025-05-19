159869e2de74b1c2f8d65ece8ee66dcb83b09485
efrain-jerv <efrain.rodriguez@ontica.org>
2025-05-19 12:41:53 -0700

    BalanceEntryDto added to adapters

---
+++ BalanceEngine/Commons/Adapters/BalanceEntryDto.cs
@@ -0,0 +1,43 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Interface adapters                      *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Data Transfer Object                    *
+*  Type     : BalanzaTradicionalDto                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Output DTO used to return a Balance Entry.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.FinancialAccounting.BalanceEngine.Adapters {
+
+  /// <summary>Output DTO used to return a Balance Entry.</summary>
+  public class BalanceEntryDto {
+
+    public string AccountNumber {
+      get; internal set;
+    }
+
+
+    public decimal InitialBalance {
+      get; internal set;
+    }
+
+
+    public decimal Debit {
+      get; internal set;
+    }
+
+
+    public decimal Credit {
+      get; internal set;
+    }
+
+
+    public decimal CurrentBalance {
+      get; internal set;
+    }
+
+  } // class BalanceEntryDto
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine.Adapters
