3bc3c25d4a93dbf51f97464f43aea0c8de017ce0
efrain-jerv <efrain.rodriguez@ontica.org>
2025-06-30 16:02:34 -0700

    Object with methods to get default exchange rate type for currencies added

---
+++ BalanceEngine/Commons/Domain/ExchangeRateForCurrencies.cs
@@ -0,0 +1,122 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Helper methods                          *
+*  Type     : ExchangeRateForCurrencies                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Helper methods to valuate currencies depending rules for balances.                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+using Empiria.Time;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Helper methods to valuate currencies depending rules for balances.</summary>
+  internal class ExchangeRateForCurrencies {
+
+    private readonly DateTime fromDate;
+    private readonly DateTime toDate;
+
+    #region Constructors and parsers
+
+    internal ExchangeRateForCurrencies(DateTime _fromDate, DateTime _toDate) {
+      Assertion.Require(_fromDate, nameof(_fromDate));
+      Assertion.Require(_toDate, nameof(_toDate));
+
+      this.fromDate = _fromDate;
+      this.toDate = _toDate;
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    internal string ExchangeRateTypeUID {
+      get; private set;
+    }
+
+
+    internal string ValuateToCurrrencyUID {
+      get; private set;
+    }
+
+
+    internal DateTime ExchangeRateDate {
+      get; private set;
+    }
+
+
+    internal FixedList<ExchangeRate> ExchangeRateList {
+      get; private set;
+    }
+
+    #endregion Properties
+
+    #region Public methods
+
+    internal void GetDefaultExchangeRate() {
+
+      DetermineExchangeRateTypeForDate();
+
+      this.ValuateToCurrrencyUID = Currency.MXN.Code;
+      this.ExchangeRateList = ExchangeRate.GetList(ExchangeRateType.Parse(this.ExchangeRateTypeUID),
+                                                                          this.ExchangeRateDate);
+    }
+
+    #endregion Public methods
+
+    #region Private methods
+
+    private void DetermineExchangeRateTypeForDate() {
+      var lastDate = toDate;
+
+      while (true) {
+
+        if (lastDate.Day == GetDaysInMonth(lastDate)) {
+
+          GetClosingExchangeRateType(lastDate);
+          break;
+        } else if (lastDate == GetLastWorkingDate(lastDate)) {
+
+          GetDailyExchangeRateType(lastDate);
+          break;
+        }
+
+        lastDate = lastDate.AddDays(-1);
+      }
+    }
+
+
+    private void GetClosingExchangeRateType(DateTime exchangeRateDate) {
+      this.ExchangeRateDate = exchangeRateDate;
+      this.ExchangeRateTypeUID = ExchangeRateType.ValorizacionBanxico.UID;
+    }
+
+
+    private void GetDailyExchangeRateType(DateTime lastWorkingDate) {
+
+      this.ExchangeRateDate = lastWorkingDate;
+      this.ExchangeRateTypeUID = ExchangeRateType.Diario.UID;
+    }
+
+
+    private int GetDaysInMonth(DateTime date) {
+
+      return DateTime.DaysInMonth(date.Year, date.Month);
+    }
+
+
+    private DateTime GetLastWorkingDate(DateTime date, bool includeDate = true) {
+
+      var calendar = EmpiriaCalendar.Default;
+      return calendar.LastWorkingDate(date, includeDate);
+    }
+
+    #endregion Private methods
+
+  } // class ExchangeRateForCurrencies
+
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
