4e16abc256a87292ebae80459593294919409f23
José Manuel Cota <jmcota@ontica.org>
2025-07-01 09:05:36 -0400

    Rfx Cash transaction adapters reordered

--- CashLedger/Adapters/CashTransactionDto.cs
+++ CashLedger/Adapters/CashTransactionDto.cs
@@ -1,14 +1,17 @@
 ﻿/* Empiria Financial *****************************************************************************************
 *                                                                                                            *
-*  Module   : Cash Ledger                                    Component : Adapters Layer                      *
-*  Assembly : FinancialAccounting.CashLedger.dll             Pattern   : Output DTO                          *
-*  Type     : CashTransactionHolderDto                       License   : Please read LICENSE.txt file        *
+*  Module   : Cash Ledger                                   Component : Adapters Layer                       *
+*  Assembly : FinancialAccounting.CashLedger.dll            Pattern   : Output DTO                           *
+*  Type     : CashTransactionHolder, Descriptor, Entry      License   : Please read LICENSE.txt file         *
 *                                                                                                            *
-*  Summary  : Output holder DTO used for a cash transaction.                                                 *
+*  Summary  : Output DTO used to retrieve cash ledger transactions.                                          *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System;
+
 namespace Empiria.FinancialAccounting.CashLedger.Adapters {
+
 
   /// <summary>Output holder DTO used for a cash transaction.</summary>
   public class CashTransactionHolderDto {
@@ -24,6 +27,59 @@ namespace Empiria.FinancialAccounting.CashLedger.Adapters {
   }  // class CashTransactionHolderDto
 
 
+  /// <summary>Output DTO used to retrieve cash ledger transactions for use in lists.</summary>
+  public class CashTransactionDescriptor {
+
+    public long Id {
+      get; internal set;
+    }
+
+    public string Number {
+      get; internal set;
+    }
+
+    public string LedgerName {
+      get; internal set;
+    }
+
+    public string Concept {
+      get; internal set;
+    }
+
+    public string TransactionTypeName {
+      get; internal set;
+    }
+
+    public string VoucherTypeName {
+      get; internal set;
+    }
+
+    public string SourceName {
+      get; internal set;
+    }
+
+    public DateTime AccountingDate {
+      get; internal set;
+    }
+
+    public DateTime RecordingDate {
+      get; internal set;
+    }
+
+    public string ElaboratedBy {
+      get; internal set;
+    }
+
+    public string Status {
+      get; internal set;
+    }
+
+    public string StatusName {
+      get; internal set;
+    }
+
+  }  // class CashTransactionDescriptor
+
 
   /// <summary>Output DTO used to retrieve cash ledger transaction entries.</summary>
   public class CashTransactionEntryDto {
@@ -34,43 +90,55 @@ namespace Empiria.FinancialAccounting.CashLedger.Adapters {
 
     public string AccountNumber {
       get; internal set;
-    } = string.Empty;
-
+    }
 
     public string AccountName {
       get; internal set;
-    } = string.Empty;
+    }
 
+    public string ParentAccountFullName {
+      get; internal set;
+    }
 
     public string SectorCode {
       get; internal set;
-    } = string.Empty;
-
+    }
 
     public string SubledgerAccountNumber {
       get; internal set;
-    } = string.Empty;
-
+    }
 
     public string SubledgerAccountName {
       get; internal set;
-    } = string.Empty;
-
+    }
 
     public string VerificationNumber {
       get; internal set;
-    } = string.Empty;
+    }
 
+    public string ResponsibilityAreaCode {
+      get; internal set;
+    }
 
     public string ResponsibilityAreaName {
       get; internal set;
-    } = string.Empty;
+    }
 
+    public string BudgetCode {
+      get; internal set;
+    }
+
+    public string Description {
+      get; internal set;
+    }
+
+    public DateTime Date {
+      get; internal set;
+    }
 
     public string CurrencyName {
       get; internal set;
-    } = string.Empty;
-
+    }
 
     public decimal ExchangeRate {
       get; internal set;
