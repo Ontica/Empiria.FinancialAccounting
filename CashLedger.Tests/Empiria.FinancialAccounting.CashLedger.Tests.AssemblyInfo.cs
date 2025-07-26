baebbfc52fd753826d8a5bc110f8477bea36a98c
José Manuel Cota <jmcota@ontica.org>
2025-07-26 10:47:56 -0400

    Cash Ledger module added to provide Cash Transaction Recording services

---
+++ CashLedger.Tests/Empiria.FinancialAccounting.CashLedger.Tests.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                        Module  : Cash Ledger Services Tests       *
+*  Assembly : Empiria.FinancialAccounting.CashLedger.Tests.dll    Pattern : Assembly Attributes File         *
+*                                                                 License : Please read LICENSE.txt file     *
+*                                                                                                            *
+*  Summary  : Tests suite for Empiria Financial Accounting Cash Ledger services.                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ CashLedger/Empiria.FinancialAccounting.CashLedger.AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                 Module  : Cash Transaction Recording Services     *
+*  Assembly : Empiria.FinancialAccounting.CashLedger.dll   Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides Empiria Financial Accounting Cash Transaction Recording services.                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Empiria.FinancialAccounting.CashLedger.Tests")]
