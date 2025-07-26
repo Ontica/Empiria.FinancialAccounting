e5c659cae215e4d947a3853971614bae17abe7b5
José Manuel Cota <jmcota@ontica.org>
2025-07-26 12:23:00 -0400

    Financial Accounting Web Api Client module added

---
+++ WebApiClient.Tests/Empiria.FinancialAccounting.WebApiClient.Tests.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                    Module  : Web Api Client Tests                 *
+*  Assembly : FinancialAccounting.WebApiClient.Tests.dll      Pattern : Assembly Attributes File             *
+*                                                             License : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Tests suite for Empiria Financial Accounting Web Api Client services.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ WebApiClient/Empiria.FinancialAccounting.WebApiClient.AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Financial Accounting                   Module  : Financial Accounting Web Api Client   *
+*  Assembly : Empiria.FinancialAccounting.WebApiClient.dll   Pattern : Assembly Attributes File              *
+*                                                            License : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides HTTP client access services for Empiria Financial Accounting Web Apis.                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Empiria.FinancialAccounting.WebApiClient.Tests")]
