c96cf297233ca0b3cfbf88d83040bc27ce22fabc
José Manuel Cota <jmcota@ontica.org>
2025-08-16 12:58:22 -0400

    Rfx Inherit CashEntryDescriptor from SharedCashEntryDescriptor

--- CashLedger/Adapters/CashEntryDescriptor.cs
+++ CashLedger/Adapters/CashEntryDescriptor.cs
@@ -8,112 +8,12 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
-using System;
+using Empiria.Financial.Integration.CashLedger;
 
 namespace Empiria.FinancialAccounting.CashLedger.Adapters {
 
   /// <summary>Output DTO used to retrieve cash ledger transaction entries for use in lists.</summary>
-  public class CashEntryDescriptor {
-
-    public long Id {
-      get; set;
-    }
-
-    public string AccountNumber {
-      get; set;
-    }
-
-    public string AccountName {
-      get; set;
-    }
-
-    public string ParentAccountFullName {
-      get; set;
-    }
-
-    public string SectorCode {
-      get; set;
-    }
-
-    public string SubledgerAccountNumber {
-      get; set;
-    }
-
-    public string SubledgerAccountName {
-      get; set;
-    }
-
-    public string VerificationNumber {
-      get; set;
-    }
-
-    public string ResponsibilityAreaCode {
-      get; set;
-    }
-
-    public string ResponsibilityAreaName {
-      get; set;
-    }
-
-    public string BudgetCode {
-      get; set;
-    }
-
-    public string Description {
-      get; set;
-    }
-
-    public DateTime Date {
-      get; set;
-    }
-
-    public int CurrencyId {
-      get; set;
-    }
-
-    public string CurrencyName {
-      get; set;
-    }
-
-    public decimal ExchangeRate {
-      get; set;
-    }
-
-    public decimal Debit {
-      get; set;
-    }
-
-    public decimal Credit {
-      get; set;
-    }
-
-    public int CashAccountId {
-      get; set;
-    }
-
-    public long TransactionId {
-      get; set;
-    }
-
-    public string TransactionNumber {
-      get; set;
-    }
-
-    public string TransactionLedgerName {
-      get; set;
-    }
-
-    public string TransactionConcept {
-      get; set;
-    }
-
-    public DateTime TransactionAccountingDate {
-      get; set;
-    }
-
-    public DateTime TransactionRecordingDate {
-      get; set;
-    }
+  public class CashEntryDescriptor : SharedCashEntryDescriptor {
 
   }  // class CashEntryDescriptor
 
