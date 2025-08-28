f48e478e3c38bb7698d8d040dc9c773993da6417
José Manuel Cota <jmcota@ontica.org>
2025-08-27 20:01:32 -0400

    Rfx Web api client services were moved from Financial.Management

---
+++ WebApiClient/Services/CashLedgerServices.cs
@@ -0,0 +1,60 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Cash Ledger                                Component : Data Layer                              *
+*  Assembly : FinancialAccounting.WebApiClient.dll       Pattern   : Web api client                          *
+*  Type     : CashLedgerServices                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides financial accounting cash ledger transactions services using a web proxy.             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Threading.Tasks;
+
+using Empiria.WebApi.Client;
+
+namespace Empiria.FinancialAccounting.ClientServices {
+
+  /// <summary>Provides financial accounting cash ledger transactions services using a web proxy.</summary>
+  public class CashLedgerServices {
+
+    private readonly WebApiClient _webApiClient;
+
+    public CashLedgerServices() {
+      _webApiClient = WebApiClient.GetInstance("SICOFIN");
+    }
+
+
+    public async Task<FixedList<NamedEntityDto>> GetAccountingLedgers() {
+
+      string path = "v2/financial-accounting/ledgers/ifrs";
+
+      return await _webApiClient.GetAsync<FixedList<NamedEntityDto>>(path);
+    }
+
+
+    public async Task<FixedList<NamedEntityDto>> GetTransactionSources() {
+
+      string path = "v2/financial-accounting/vouchers/functional-areas";
+
+      return await _webApiClient.GetAsync<FixedList<NamedEntityDto>>(path);
+    }
+
+
+    public async Task<FixedList<NamedEntityDto>> GetTransactionTypes() {
+
+      string path = "v2/financial-accounting/vouchers/transaction-types";
+
+      return await _webApiClient.GetAsync<FixedList<NamedEntityDto>>(path);
+    }
+
+
+    public async Task<FixedList<NamedEntityDto>> GetVoucherTypes() {
+
+      string path = "v2/financial-accounting/vouchers/voucher-types";
+
+      return await _webApiClient.GetAsync<FixedList<NamedEntityDto>>(path);
+    }
+
+  }  // class CashLedgerServices
+
+}  // namespace Empiria.FinancialAccounting.ClientServices
---
+++ WebApiClient/Services/CashTransactionServices.cs
@@ -0,0 +1,95 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Cash Ledger                                Component : Data Layer                              *
+*  Assembly : FinancialAccounting.WebApiClient.dll       Pattern   : Web api client                          *
+*  Type     : CashTransactionServices                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides financial accounting cash ledger transactions services using a web proxy.             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Threading.Tasks;
+
+using Empiria.Storage;
+using Empiria.WebApi.Client;
+
+using Empiria.CashFlow.CashLedger.Adapters;
+
+namespace Empiria.FinancialAccounting.ClientServices {
+
+  /// <summary>Provides financial accounting cash ledger transactions services using a web proxy.</summary>
+  public class CashTransactionServices {
+
+    private readonly WebApiClient _webApiClient;
+
+    public CashTransactionServices() {
+      _webApiClient = WebApiClient.GetInstance("SICOFIN");
+    }
+
+
+    public async Task<T> GetTransaction<T>(long id) {
+
+      string path = $"v2/financial-accounting/cash-ledger/transactions/{id}";
+
+      return await _webApiClient.GetAsync<T>(path);
+    }
+
+
+    public async Task<FileDto> GetTransactionAsPdfFile(long id) {
+
+      string path = $"v2/financial-accounting/vouchers/{id}/print";
+
+      return await _webApiClient.GetAsync<FileDto>(path);
+    }
+
+
+    public async Task<FixedList<T>> GetTransactions<T>(FixedList<long> transactionIds) {
+
+      string path = $"v2/financial-accounting/cash-ledger/transactions/bulk-operation/get-transactions";
+
+      return await _webApiClient.PostAsync<FixedList<T>>(transactionIds, path);
+    }
+
+
+    public async Task<FixedList<CashEntryDescriptor>> GetTransactionsEntries(FixedList<long> entriesIds) {
+
+      string path = $"v2/financial-accounting/cash-ledger/entries/bulk-operation/get-entries";
+
+      return await _webApiClient.PostAsync<FixedList<CashEntryDescriptor>>(entriesIds, path);
+    }
+
+
+    public async Task<FixedList<CashEntryDescriptor>> SearchEntries(BaseCashLedgerQuery query) {
+
+      string path = "v2/financial-accounting/cash-ledger/entries/search";
+
+      return await _webApiClient.PostAsync<FixedList<CashEntryDescriptor>>(query, path);
+    }
+
+
+    public async Task<FixedList<CashTransactionDescriptor>> SearchTransactions(BaseCashLedgerQuery query) {
+
+      string path = "v2/financial-accounting/cash-ledger/transactions/search";
+
+      return await _webApiClient.PostAsync<FixedList<CashTransactionDescriptor>>(query, path);
+    }
+
+
+    public async Task UpdateBulkEntries(FixedList<CashEntryFields> bulkEntries) {
+
+      string path = $"v2/financial-accounting/cash-ledger/transactions/bulk-operation/update-entries";
+
+      await _webApiClient.PostAsync(bulkEntries, path);
+    }
+
+
+    public async Task<T> UpdateEntries<T>(FixedList<CashEntryFields> entries) {
+
+      string path = $"v2/financial-accounting/cash-ledger/transactions/{entries[0].TransactionId}/update-entries";
+
+      return await _webApiClient.PostAsync<T>(entries, path);
+    }
+
+  }  // class CashTransactionServices
+
+}  // namespace Empiria.FinancialAccounting.ClientServices
