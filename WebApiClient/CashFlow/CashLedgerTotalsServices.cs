28e190bd70986536881976ef0918d4481e0515ae
José Manuel Cota <jmcota@ontica.org>
2025-08-27 22:09:26 -0400

    CashLedgerTotalsServices.GetCashLedgerTotals() wep api client added

---
+++ WebApiClient/CashFlow/CashLedgerTotalsServices.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Cash Ledger                                Component : Data Layer                              *
+*  Assembly : FinancialAccounting.WebApiClient.dll       Pattern   : Web api client                          *
+*  Type     : CashLedgerTotalsServices                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides financial accounting cash ledger totals services using a web proxy.                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Threading.Tasks;
+
+using Empiria.WebApi.Client;
+
+using Empiria.CashFlow.CashLedger.Adapters;
+
+namespace Empiria.FinancialAccounting.ClientServices {
+
+  /// <summary>Provides financial accounting cash ledger totals services using a web proxy.</summary>
+  public class CashLedgerTotalsServices {
+
+    private readonly WebApiClient _webApiClient;
+
+    public CashLedgerTotalsServices() {
+      _webApiClient = WebApiClient.GetInstance("SICOFIN");
+    }
+
+
+    public async Task<FixedList<CashLedgerTotalEntryDto>> GetCashLedgerTotals(BaseCashLedgerTotalsQuery query) {
+
+      string path = "v2/financial-accounting/cash-ledger/totals";
+
+      return await _webApiClient.PostAsync<FixedList<CashLedgerTotalEntryDto>>(query, path);
+    }
+
+  }  // class CashLedgerTotalsServices
+
+}  // namespace Empiria.FinancialAccounting.ClientServices
