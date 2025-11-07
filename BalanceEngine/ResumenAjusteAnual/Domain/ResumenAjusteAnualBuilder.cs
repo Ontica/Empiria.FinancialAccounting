e02c0dc2cfd0c0c45128f541ef037baa5b83d635
efrain-jerv <efrain.rodriguez@ontica.org>
2025-11-06 18:53:50 -0700

    Domain layer for ResumenAjusteAnual report added

---
+++ BalanceEngine/ResumenAjusteAnual/Domain/ResumenAjusteAnualBuilder.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Builder                                 *
+*  Type     : AnaliticoDeCuentasBuilder                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Genera los datos para el reporte Resumen de ajuste anual.                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Empiria.FinancialAccounting.BalanceEngine.Adapters;
+using Empiria.FinancialAccounting.BalanceEngine.Data;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Genera los datos para el reporte Resumen de ajuste anual</summary>
+  internal class ResumenAjusteAnualBuilder {
+
+    private readonly TrialBalanceQuery _query;
+
+    internal ResumenAjusteAnualBuilder(TrialBalanceQuery query) {
+      _query = query;
+    }
+
+
+    internal FixedList<ResumenAjusteAnualEntry> Build() {
+
+      FixedList<TrialBalanceEntry> baseAccountEntries = BalancesDataService.GetTrialBalanceEntries(_query);
+
+      return Build(baseAccountEntries);
+    }
+
+
+    internal FixedList<ResumenAjusteAnualEntry> Build(FixedList<TrialBalanceEntry> baseAccountEntries) {
+
+
+
+      return new FixedList<ResumenAjusteAnualEntry>();
+    }
+
+
+  } // class ResumenAjusteAnualBuilder
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
---
+++ BalanceEngine/ResumenAjusteAnual/Domain/ResumenAjusteAnualEntry.cs
@@ -0,0 +1,21 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Information Holder                      *
+*  Type     : AnalyticBalanceEntry                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an entry for resumen de ajuste anual entry.                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Represents an entry for resumen de ajuste anual entry</summary>
+  public class ResumenAjusteAnualEntry : ITrialBalanceEntry {
+
+
+
+  } // class ResumenAjusteAnualEntry
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
