8381661f0327d9d647829dc8144a22f2ec2a4a21
efrain-jerv <efrain.rodriguez@ontica.org>
2025-11-11 18:49:56 -0700

    ResumenAjusteAnualEntry class added to domain layer

---
+++ BalanceEngine/ResumenAjusteAnual/Domain/ResumenAjusteAnualEntry.cs
@@ -0,0 +1,92 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Information Holder                      *
+*  Type     : ResumenAjusteEntry                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an entry for resumen de ajuste anual entry.                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Represents an entry for resumen de ajuste anual entry</summary>
+  internal class ResumenAjusteAnualEntry : ITrialBalanceEntry {
+
+    public DateTime FiscalYearDate {
+      get; internal set;
+    }
+
+
+    public decimal Debit {
+      get; internal set;
+    }
+
+
+    public decimal Credit {
+      get; internal set;
+    }
+
+
+    public decimal AverageBalanceCredit {
+      get; internal set;
+    }
+
+
+    public decimal AverageBalanceDebit {
+      get; internal set;
+    }
+
+
+    /// <summary>Credit > Debit</summary>
+    public decimal DeductibleAdjustment {
+      get; internal set;
+    }
+
+
+    /// <summary>Debit > Credit</summary>
+    public decimal CumulativeAdjustment {
+      get; internal set;
+    }
+
+
+    /// <summary>Cumulative/Deductible</summary>
+    public decimal InflationAdjustmentMonths {
+      get; internal set;
+    }
+
+
+    /// <summary>Cumulative/Deductible</summary>
+    public decimal InflationAdjustment12Months {
+      get; internal set;
+    }
+
+
+    public decimal INPCLastMonthPreviousYear {
+      get; internal set;
+    }
+
+
+    public decimal INPCPreviousYear {
+      get; internal set;
+    }
+
+
+    public decimal INPC {
+      get; internal set;
+    }
+
+
+    public decimal FactorMonthsElapsed {
+      get; internal set;
+    }
+
+
+    public decimal Factor12Months {
+      get; internal set;
+    }
+
+  } // class ResumenAjusteAnualEntry
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
