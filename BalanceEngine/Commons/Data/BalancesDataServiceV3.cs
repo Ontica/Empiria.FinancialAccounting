ef7a98d2644ac8ff7863b7bdfbcb71726f40f240
José Manuel Cota <jmcota@ontica.org>
2026-04-01 11:09:31 -0400

    Rfx TrialBalanceValued

---
+++ BalanceEngine/Commons/Data/BalancesDataServiceV3.cs
@@ -0,0 +1,43 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Data Layer                              *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Data Service                            *
+*  Type     : BalancesDataServiceV3                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Data services to retrieve accounting balances (version 3.0).                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Data;
+
+namespace Empiria.FinancialAccounting.BalanceEngine.Data {
+
+  /// <summary>Data services to retrieve accounting balances (version 3.0).</summary>
+  static internal class BalancesDataServiceV3 {
+
+    #region Methods
+
+    static internal FixedList<TrialBalanceValued> GetDailyMovements(DateTime fromDate, DateTime toDate) {
+
+      var sql =
+        "SELECT id_cuenta_estandar, fecha_afectacion, id_moneda, " +
+               "SUM(debe) Debe, SUM(haber) Haber " +
+        "FROM vw_cof_movimiento " +
+        $"WHERE " +
+            $"fecha_afectacion >= {DataCommonMethods.FormatSqlDbDate(fromDate)} AND " +
+            $"fecha_afectacion < {DataCommonMethods.FormatSqlDbDate(toDate.AddDays(1))} AND " +
+            "id_moneda <> 1 " +
+        "GROUP BY " +
+            "id_cuenta_estandar, fecha_afectacion, id_moneda";
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetPlainObjectFixedList<TrialBalanceValued>(op);
+    }
+
+    #endregion Methods
+
+  } // class BalancesDataServiceV3
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine.Data
--- BalanceEngine/Commons/Domain/TrialBalanceValued.cs
+++ BalanceEngine/Commons/Domain/TrialBalanceValued.cs
@@ -7,28 +7,33 @@
 *  Summary  : Contains entries from TrialBalanceValued.                                                      *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
 using System;
-using Empiria.FinancialAccounting.BalanceEngine.Data;
 
 namespace Empiria.FinancialAccounting.BalanceEngine {
 
   /// <summary>Contains entries from TrialBalanceValued.</summary>
   public class TrialBalanceValued {
 
-    #region Constructors and parsers
-
-    protected TrialBalanceValued() {
-      // Require by Empiria FrameWork
-    }
-
-    #endregion Constructors and parsers
-
     #region Properties
 
     [DataField("ID_CUENTA_ESTANDAR", ConvertFrom = typeof(long))]
-    public int IdCuentaEstandar {
-      get; set;
-    } = 0;
+    internal StandardAccount CuentaEstandar {
+      get; private set;
+    }
+
+
+    public string NumeroCuenta {
+      get {
+        return CuentaEstandar.Number;
+      }
+    }
+
+    public string NombreCuenta {
+      get {
+        return CuentaEstandar.Name;
+      }
+    }
 
     [DataField("FECHA_AFECTACION", ConvertFrom = typeof(DateTime))]
     public DateTime FechaAfectacion {
@@ -36,23 +41,26 @@ namespace Empiria.FinancialAccounting.BalanceEngine {
     } = ExecutionServer.DateMinValue;
 
 
-    [DataField("NATURALEZA", Default = DebtorCreditorType.Deudora)]
     public DebtorCreditorType Naturaleza {
-      get; private set;
-    } = DebtorCreditorType.Deudora;
-
-    [DataField("ID_CUENTA_AUXILIAR", ConvertFrom = typeof(long))]
-    public long IdCuentaAuxiliar {
-      get; private set;
+      get {
+        return CuentaEstandar.DebtorCreditor;
+      }
     }
 
-    [DataField("ID_SECTOR", ConvertFrom = typeof(long))]
-    public Sector Sector {
-      get; private set;
-    }
 
     [DataField("ID_MONEDA", ConvertFrom = typeof(decimal))]
-    public Currency Currency {
+    internal Currency Currency {
+      get; private set;
+    }
+
+
+    public string CodigoMoneda {
+      get {
+        return Currency.ISOCode;
+      }
+    }
+
+    public decimal SaldoInicial {
       get; private set;
     }
 
@@ -66,33 +74,66 @@ namespace Empiria.FinancialAccounting.BalanceEngine {
       get; private set;
     }
 
-    [DataField("EXCHANGE_RATE", ConvertFrom = typeof(decimal))]
+    public decimal SaldoFinal {
+      get {
+        if (Naturaleza == DebtorCreditorType.Deudora) {
+          return SaldoInicial + Debe - Haber;
+        } else {
+          return SaldoInicial - Debe + Haber;
+        }
+      }
+    }
+
     public decimal TipoCambio {
       get; private set;
     }
 
-    [DataField("DEBE_VAL", ConvertFrom = typeof(decimal))]
-    public decimal DebeValorizado {
-      get; private set;
+
+    public decimal SaldoInicialMXN {
+      get {
+        return SaldoInicial * TipoCambio;
+      }
     }
 
-    [DataField("HABER_VAL", ConvertFrom = typeof(decimal))]
-    public decimal HaberValorizado {
-      get; private set;
+    public decimal DebeMXN {
+      get {
+        return Debe * TipoCambio;
+      }
+    }
+
+    public decimal HaberMXN {
+      get {
+        return Haber * TipoCambio;
+      }
+    }
+
+    public decimal SaldoFinalMXN {
+      get {
+        return SaldoFinal * TipoCambio;
+      }
     }
 
     #endregion Properties
 
-    #region Public methods
+    #region Methods
 
-    static public FixedList<TrialBalanceValued> GetTrialBalanceValuedTransactions(DateTime fromDate, DateTime toDate, int ExchangeRateTypeId) {
+    internal void SetExchangeRate(FixedList<ExchangeRate> exchangeRates,
+                                  DateTime initialDate, DateTime endDate) {
+      int lastDay = DateTime.DaysInMonth(FechaAfectacion.Year, FechaAfectacion.Month);
 
-      return TrialBalanceValuedDataService.GetTransactions(fromDate, toDate, ExchangeRateTypeId);
+      if (FechaAfectacion.Day == lastDay) {
+        var exchangeRate = exchangeRates.Find(x => x.Date == FechaAfectacion && x.ToCurrency.Equals(Currency) &&
+                                                   x.ExchangeRateType.Equals(ExchangeRateType.ValorizacionBanxico));
+        TipoCambio = exchangeRate.Value;
+      } else {
+        var exchangeRate = exchangeRates.Find(x => x.Date == FechaAfectacion &&
+                                                   x.ToCurrency.Equals(Currency) &&
+                                                   x.ExchangeRateType.Equals(ExchangeRateType.Diario));
+        TipoCambio = exchangeRate.Value;
+      }
     }
 
-
-    #endregion Public methods
-
+    #endregion Methods
 
   } // class TrialBalanceValued
 
