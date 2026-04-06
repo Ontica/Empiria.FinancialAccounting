59baa7bc7037cb2b729efb666f3d674c7efec180
José Manuel Cota <jmcota@ontica.org>
2026-04-05 20:06:10 -0400

    Rfx TrialBalanceValued type was renamed to MovimientosPorDia

--- BalanceEngine.Tests/Commons/BalanzaTradicionalEntriesTests.cs
+++ BalanceEngine.Tests/Commons/BalanzaTradicionalEntriesTests.cs
@@ -8,11 +8,7 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
-using System;
-
-using Xunit;
 using Empiria.Tests;
-using Empiria.FinancialAccounting.BalanceEngine.Data;
 
 namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaBalorizadaPorDia {
 
@@ -28,28 +24,6 @@ namespace Empiria.FinancialAccounting.Tests.BalanceEngine.BalanzaBalorizadaPorDi
     #endregion Initialization
 
     #region Facts
-
-    [Fact]
-    public void Should_Get_TrialBalaceValuedEntries_By_Date() {
-
-      DateTime fromDate = Convert.ToDateTime("01/01/2026");
-      DateTime toDate = Convert.ToDateTime("31/01/2026");
-
-      var sut = BalancesDataServiceV3.GetDailyMovements(fromDate, toDate);
-
-      Assert.NotNull(sut);
-    }
-
-    #endregion Facts
-
-    #region Helpers
-
-
-
-
-
-
-    #endregion Helpers
 
   } // class TrialBalanceValuedTests
 
---
+++ BalanceEngine/BalanzaValorizada/MovimientosPorDia.cs
@@ -0,0 +1,77 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Balance Engine                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.BalanceEngine.dll      Pattern   : Information Holder                      *
+*  Type     : MovimientosPorDia                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Contiene la suma de cargos y abonos por fecha y cuenta.                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+using Empiria.Data;
+using Empiria.Time;
+
+namespace Empiria.FinancialAccounting.BalanceEngine {
+
+  /// <summary>Contiene la suma de cargos y abonos por fecha y cuenta.</summary>
+  public class MovimientosPorDia {
+
+    #region Constructors and parsers
+
+    static internal FixedList<MovimientosPorDia> GetList(TimePeriod period) {
+
+      var sql =
+        "SELECT id_cuenta_estandar, id_moneda, fecha_afectacion, " +
+               "SUM(debe) Cargos, SUM(haber) Abonos " +
+        "FROM vw_cof_movimiento " +
+        $"WHERE " +
+            $"fecha_afectacion >= {DataCommonMethods.FormatSqlDbDate(period.StartTime)} AND " +
+            $"fecha_afectacion < {DataCommonMethods.FormatSqlDbDate(period.EndTime.AddDays(1))}" +
+        "GROUP BY " +
+            "id_cuenta_estandar, id_moneda, fecha_afectacion";
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetPlainObjectFixedList<MovimientosPorDia>(op);
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    [DataField("ID_CUENTA_ESTANDAR", ConvertFrom = typeof(long))]
+    internal StandardAccount CuentaEstandar {
+      get; private set;
+    }
+
+
+    [DataField("ID_MONEDA", ConvertFrom = typeof(decimal))]
+    internal Currency Moneda {
+      get; private set;
+    }
+
+
+    [DataField("FECHA_AFECTACION", ConvertFrom = typeof(DateTime))]
+    public DateTime FechaAfectacion {
+      get; private set;
+    }
+
+
+    [DataField("CARGOS", ConvertFrom = typeof(decimal))]
+    public decimal Cargos {
+      get; private set;
+    }
+
+
+    [DataField("ABONOS", ConvertFrom = typeof(decimal))]
+    public decimal Abonos {
+      get; private set;
+    }
+
+    #endregion Properties
+
+  } // class MovimientosPorDia
+
+} // namespace Empiria.FinancialAccounting.BalanceEngine
