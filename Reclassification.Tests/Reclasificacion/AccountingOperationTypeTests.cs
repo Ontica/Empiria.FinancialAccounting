c65a88be8606d6d371806d98418c8d4b9d6f980f
Christian Yáñez <cyanez@ontica.org>
2026-05-30 20:22:07 -0600

    AccountingOperationType class added

---
+++ Reclassification.Tests/Reclasificacion/AccountingOperationTypeTests.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Reclassification Services                    Component : Test cases                            *
+*  Assembly : FinancialAccounting.Reclassification.Tests   Pattern   : Unit tests                            *
+*  Type     : AccountingOperationTypeTests                 License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Unit tests for Accounting operation types.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.FinancialAccounting.Reclassification;
+
+namespace Empiria.FinancialAccounting.Tests.Reclassification {
+
+  /// <summary>Unit tests for Accounting operation types.</summary>
+  public class AccountingOperationTypeTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Parse_All_Credit_Types() {
+      var creditTypes = AccountingOperationType.GetList();
+
+      foreach (var sut in creditTypes) {
+        Assert.NotEmpty(sut.Name);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Read_All_Credit_Types() {
+      var sut = AccountingOperationType.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+    #endregion Facts
+
+  } // class AccountingOperationTypeTests
+
+} // namespace Empiria.FinancialAccounting.Tests.Reclassification
---
+++ Reclassification/Reclasificacion/Domain/AccountingOperationType.cs
@@ -0,0 +1,43 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Reclasfication                             Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.Core.dll               Pattern   : Empiria General Object                  *
+*  Type     : AccountingOperationType                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an accouting operation type.                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+namespace Empiria.FinancialAccounting.Reclassification {
+
+  /// <summary>Represents an accouting operation type.</summary>
+  public class AccountingOperationType : GeneralObject {
+
+    #region Constructors and parsers
+
+    protected AccountingOperationType() {
+      // Required by Empiria Framework.
+    }
+
+    static public AccountingOperationType Parse(int id) {
+      return BaseObject.ParseId<AccountingOperationType>(id);
+    }
+
+
+    static public AccountingOperationType Parse(string uid) {
+      return BaseObject.ParseKey<AccountingOperationType>(uid);
+    }
+
+
+    static public FixedList<AccountingOperationType> GetList() {
+      return BaseObject.GetList<AccountingOperationType>()
+                       .ToFixedList();
+    }
+
+    static public AccountingOperationType Empty => BaseObject.ParseEmpty<AccountingOperationType>();
+
+    #endregion Constructors and parsers
+
+
+  } // class AccountingOperationType
+
+}  // namespace Empiria.FinancialAccounting
