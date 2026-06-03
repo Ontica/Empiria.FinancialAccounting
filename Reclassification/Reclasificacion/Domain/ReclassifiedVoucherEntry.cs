31d5e1176b03ace7a811091d317f220b4d521d15
José Manuel Cota <jmcota@ontica.org>
2026-06-02 20:57:00 -0400

    Rfx VoucherEntriesReclassificator

---
+++ Reclassification/Reclasificacion/Domain/ReclassifiedVoucherEntry.cs
@@ -0,0 +1,69 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Reclassification Services                  Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.Reclassification.dll   Pattern   : Information Holder                      *
+*  Type     : ReclassifiedVoucherEntry                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds information for a reclassified accounting voucher entry.                                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.FinancialAccounting.Vouchers;
+
+namespace Empiria.FinancialAccounting.Reclassification {
+
+  /// <summary>Used to load ReclassifiedVoucherEntry.</summary>
+  public class ReclassifiedVoucherEntry {
+
+    #region Constructors and Parsers
+
+    internal ReclassifiedVoucherEntry(string UID, VoucherEntry entry, AccountingRule rule) {
+      this.UID = UID;
+      this.VoucherEntry = entry;
+      this.OperationType = rule.AccountingOperationType;
+      this.AccountingRule = rule;
+    }
+
+    #endregion Constructors and Parsers
+
+    #region Properties
+
+    public string UID {
+      get;
+    }
+
+    internal VoucherEntry VoucherEntry {
+      get;
+    }
+
+    public AccountingOperationType OperationType {
+      get;
+    }
+
+    public AccountingRule AccountingRule {
+      get;
+    }
+
+    public Currency NewCurrency {
+      get; private set;
+    } = Currency.Empty;
+
+
+    public decimal NewAmount {
+      get; private set;
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    internal void ReclassifyCurrency(Currency newCurrency, decimal newAmount) {
+      this.NewCurrency = newCurrency;
+      this.NewAmount = newAmount;
+    }
+
+    #endregion Methods
+
+  } // class ReclassifiedVoucherEntry
+
+} // namespace Empiria.FinancialAccounting.Reclassification
---
+++ Reclassification/Reclasificacion/Domain/VoucherEntriesReclassificator.cs
@@ -0,0 +1,168 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Reclassification Services                  Component : Domain Layer                            *
+*  Assembly : FinancialAccounting.Reclassification.dll   Pattern   : Information Holder                      *
+*  Type     : VoucherEntriesReclassificator              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Reclassifies the entries of an accounting voucher in operative transactions                    *
+*             using a set of accounting rules.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+using System.Collections.Generic;
+
+using Empiria.FinancialAccounting.Vouchers;
+
+using Empiria.FinancialAccounting.Reclassification.Data;
+
+namespace Empiria.FinancialAccounting.Reclassification {
+
+  /// <summary>Reclassifies the entries of an accounting voucher in
+  /// operative transactions using a set of accounting rules.</summary>
+  public class VoucherEntriesReclassificator {
+
+    static readonly FixedList<AccountingRule> RULES = AccountingRule.GetList();
+
+    private readonly FixedList<VoucherEntry> _toProcessEntries;
+
+    #region Constructors and parsers
+
+    internal VoucherEntriesReclassificator(Voucher voucher) {
+      Assertion.Require(voucher, nameof(voucher));
+
+      _toProcessEntries = new FixedList<VoucherEntry>(voucher.Entries);
+    }
+
+    #endregion Constructors and parsers
+
+    #region Methods
+
+    public void Execute() {
+
+      foreach (var rule in RULES) {
+
+        var debitEntries = FindDebitEntries(rule);
+
+        foreach (var entry in debitEntries) {
+          Group(entry, rule);
+        }
+      }
+    }
+
+    #endregion Methods
+
+    #region Helpers
+
+    private void Group(VoucherEntry debitEntry, AccountingRule rule) {
+
+      var creditEntry = TryFindCreditEntry(rule, debitEntry);
+
+      if (creditEntry == null) {
+        return;
+      }
+
+      var entriesGroup = new List<VoucherEntry> {
+        debitEntry, creditEntry
+      };
+
+
+      if (debitEntry.Currency.Equals(creditEntry.Currency)) {
+        ProcessReclassifiedEntries(entriesGroup, rule);
+        return;
+      }
+
+      if (debitEntry.Currency.Equals(Currency.UDI)) {
+
+        var entryUDISDebe = TryFindEntry("9.01.03", debitEntry, isDebit: true);
+
+        if (entryUDISDebe == null) {
+          return;
+        }
+
+        var entryUDISHaber = TryFindEntry("9.01.04", debitEntry, isDebit: false);
+
+        if (entryUDISHaber == null) {
+          return;
+        }
+
+        entriesGroup.Add(entryUDISDebe);
+        entriesGroup.Add(entryUDISHaber);
+
+      } else {
+
+        var entryMExtDebe = TryFindEntry("9.01.01", debitEntry, isDebit: true);
+
+        if (entryMExtDebe == null) {
+          return;
+        }
+
+        var entryMExtHaber = TryFindEntry("9.01.02", debitEntry, isDebit: false);
+
+        if (entryMExtHaber == null) {
+          return;
+        }
+
+        entriesGroup.Add(entryMExtDebe);
+        entriesGroup.Add(entryMExtHaber);
+      }
+
+      ProcessReclassifiedEntries(entriesGroup, rule);
+    }
+
+
+    private void ProcessReclassifiedEntries(List<VoucherEntry> entries, AccountingRule rule) {
+
+      string uid = Guid.NewGuid().ToString();
+
+      var reclassifiedEntries = new List<ReclassifiedVoucherEntry>();
+
+      foreach (var entry in entries) {
+
+        var reclassifiedEntry = new ReclassifiedVoucherEntry(uid, entry, rule);
+
+        reclassifiedEntries.Add(reclassifiedEntry);
+
+        _toProcessEntries.Remove(entry);
+      }
+
+      reclassifiedEntries[0].ReclassifyCurrency(reclassifiedEntries[0].VoucherEntry.Currency,
+                                                reclassifiedEntries[0].VoucherEntry.Debit);
+
+      reclassifiedEntries[1].ReclassifyCurrency(reclassifiedEntries[0].VoucherEntry.Currency,
+                                                reclassifiedEntries[0].VoucherEntry.Debit);
+
+      ReclassificationDataService.UpdateReclassifiedVoucherEntries(reclassifiedEntries.ToFixedList());
+    }
+
+
+
+    private bool AmountsMatch(decimal a, decimal b) {
+      return Math.Round(a, 2) == Math.Round(b, 2);
+    }
+
+
+    private VoucherEntry TryFindEntry(string accountNumber, VoucherEntry cuenta, bool isDebit) {
+      return _toProcessEntries.Find(x => x.LedgerAccount.Number == accountNumber &&
+                                         AmountsMatch(isDebit ? x.Debit * x.ExchangeRate : x.Credit * x.ExchangeRate,
+                                                      cuenta.ExchangeRate * cuenta.Debit));
+    }
+
+
+    private VoucherEntry TryFindCreditEntry(AccountingRule operation, VoucherEntry debitEntry) {
+      return _toProcessEntries.Find(x => x.LedgerAccount.Number == operation.CreditAccount &&
+                                         x.SubledgerAccount.Number == debitEntry.SubledgerAccount.Number &&
+                                         AmountsMatch(x.Credit * x.ExchangeRate,
+                                                      debitEntry.ExchangeRate * debitEntry.Debit));
+    }
+
+
+    private FixedList<VoucherEntry> FindDebitEntries(AccountingRule operation) {
+      return _toProcessEntries.FindAll(x => x.LedgerAccount.Number == operation.DebitAccount);
+    }
+
+    #endregion Helpers
+
+  } // class VoucherEntriesReclassificator
+
+} // namespace Empiria.FinancialAccounting.Reclassification
