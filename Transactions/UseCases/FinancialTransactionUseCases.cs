cd19e81424935793f12096b61a1720f6b6b54204
José Manuel Cota <jmcota@ontica.org>
2026-09-17 11:35:15 -0400

    FinancialTransactionUseCases.PostTransaction() use case and web api added

---
+++ Transactions/UseCases/FinancialTransactionUseCases.cs
@@ -0,0 +1,49 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Transactions Services            Component : Use cases Layer                         *
+*  Assembly : FinancialAccounting.Transactions.dll       Pattern   : Use case interactor class               *
+*  Type     : FinancialTransactionUseCases               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases used to process external financial transactions as accounting transactions.          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Financial.Transactions.Adapters;
+using Empiria.Json;
+
+namespace Empiria.FinancialAccounting.Transactions.UseCases {
+
+  /// <summary>Use cases used to process external financial transactions as accounting transactions.</summary>
+  public class FinancialTransactionUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected FinancialTransactionUseCases() {
+      // no-op
+    }
+
+
+    static public FinancialTransactionUseCases UseCaseInteractor() {
+      return CreateInstance<FinancialTransactionUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public int PostTransaction(FinancialTransactionDto fields) {
+
+      JsonObject payload = JsonObject.Parse(fields.Payload);
+
+      EmpiriaLog.Debug($"Payload recibido de {fields.TransactionId}: {fields.Payload.ToString()}");
+
+      return EmpiriaMath.GetRandom(1, 100);
+    }
+
+    #endregion Use cases
+
+  } // class FinancialTransactionUseCases
+
+} // Empiria.FinancialAccounting.Transactions.UseCases
---
+++ WebApi/Transactions/FinancialTransactionsController.cs
@@ -0,0 +1,43 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Accounting Transactions            Component : Web Api                               *
+*  Assembly : Empiria.FinancialAccounting.WebApi.dll       Pattern   : Controller                            *
+*  Type     : FinancialTransactionsController              License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used to integrate financial transactions with financial accounting processes.          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.Financial.Transactions.Adapters;
+
+using Empiria.FinancialAccounting.Transactions.UseCases;
+
+namespace Empiria.FinancialAccounting.Transactions.WebApi {
+
+  /// <summary>Web API used to integrate financial transactions with financial accounting processes.</summary>
+  public class FinancialTransactionsController : WebApiController {
+
+    #region Web Apis
+
+    [HttpPost]
+    [Route("v2/financial-accounting/transactions")]
+    public SingleObjectModel PostTransaction([FromBody] FinancialTransactionDto fields) {
+
+      // base.RequireBody(fields);
+
+      using (var usecases = FinancialTransactionUseCases.UseCaseInteractor()) {
+        int result = usecases.PostTransaction(fields);
+
+        return new SingleObjectModel(base.Request, result);
+      }
+    }
+
+    #endregion Web Apis
+
+  }  // class FinancialTransactionsController
+
+}  // namespace Empiria.FinancialAccounting.Transactions.WebApi
